a3f81be7454a20eb05d222f74495073180091ae2
Mads R. B. Kristensen <madsbk@gmail.com>
2024-12-10 14:45:56 +0100

    use mpirun

--- ci/run_cpp_benchmark_smoketests.sh
+++ ci/run_cpp_benchmark_smoketests.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/benchmarks/librapidsmp/"
 
 # Ensure that benchmarks are runnable
-./bench_shuffle
+OMPI_MCA_opal_cuda_support=1 mpirun --allow-run-as-root -np 3 ./bench_shuffle
--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -6,5 +6,16 @@ set -euo pipefail
 # Support customizing the ctests' install location
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/tests/librapidsmp/"
 
-# Run gtests
+# Run gtests (single rank)
 ctest --no-tests=error --output-on-failure "$@"
+
+# Run gtests with mpirun
+set -x
+export OMPI_MCA_opal_cuda_support=1  # enable CUDA support
+# Note, we run with many different number of ranks, which we can do as long as
+# the test suite only takes seconds to run.
+mpirun --allow-run-as-root -np 2 ctest --no-tests=error --output-on-failure "$@"
+mpirun --allow-run-as-root -np 3 ctest --no-tests=error --output-on-failure "$@"
+mpirun --allow-run-as-root -np 4 ctest --no-tests=error --output-on-failure "$@"
+mpirun --allow-run-as-root -np 5 ctest --no-tests=error --output-on-failure "$@"
+mpirun --allow-run-as-root -np 8 ctest --no-tests=error --output-on-failure "$@"
