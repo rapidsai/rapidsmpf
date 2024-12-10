8d0dfcab9dd01a49cc227c0562a585c58f6d9f73
Mads R. B. Kristensen <madsbk@gmail.com>
2024-12-10 08:48:18 +0100

    CI: build cpp (#15)
    Closes https://github.com/rapidsai/rapids-multi-gpu/issues/2
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/rapids-multi-gpu/pull/15

---
+++ ci/build_cpp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2024, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+rapids-configure-conda-channels
+
+source rapids-configure-sccache
+
+source rapids-date-string
+
+export CMAKE_GENERATOR=Ninja
+
+rapids-print-env
+
+rapids-logger "Begin cpp build"
+
+sccache --zero-stats
+
+RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry mambabuild \
+    conda/recipes/librapidsmp
+
+sccache --show-adv-stats
+
+rapids-upload-conda-to-s3 cpp
