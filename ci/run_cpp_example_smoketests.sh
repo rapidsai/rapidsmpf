acfc84b4c59e6ac99a48a639e0d1c6dded31bc99
Mads R. B. Kristensen <madsbk@gmail.com>
2024-12-10 15:58:29 +0100

    typo

--- ci/run_cpp_example_smoketests.sh
+++ ci/run_cpp_example_smoketests.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/examples/librapidsmp/"
 
 # Ensure that benchmarks are runnable
-OMPI_MCA_opal_cuda_support=1 mpirun --allow-run-as-root -np 2 ./bench_shuffle
+OMPI_MCA_opal_cuda_support=1 mpirun --allow-run-as-root -np 2 ./example_shuffle
