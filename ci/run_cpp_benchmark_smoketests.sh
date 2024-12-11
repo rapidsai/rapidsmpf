af18d30a34713b66c2d4b23ada2daae317304953
Mads R. B. Kristensen <madsbk@gmail.com>
2024-12-11 09:46:07 +0100

    OMPI_ALLOW_RUN_AS_ROOT

--- ci/run_cpp_benchmark_smoketests.sh
+++ ci/run_cpp_benchmark_smoketests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
-set -euo pipefail
+set -xeuo pipefail
 
 # Support customizing the ctests' install location
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/benchmarks/librapidsmp/"
 
-# Ensure that benchmarks are runnable
-set -x
+# OpenMPI specific options
+export OMPI_ALLOW_RUN_AS_ROOT=1  # CI runs as root
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export OMPI_MCA_opal_cuda_support=1  # enable CUDA support in OpenMPI
+
+# Ensure that benchmarks are runnable
 mpirun -np 3 ./bench_shuffle
