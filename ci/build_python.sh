502ceff68b7119390e52301c93f876f3feb883c9
Mads R. B. Kristensen <madsbk@gmail.com>
2025-01-09 16:17:41 +0100

    conda/recipes/rapidsmp

---
+++ ci/build_python.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+rapids-configure-conda-channels
+
+source rapids-configure-sccache
+
+source rapids-date-string
+
+export CMAKE_GENERATOR=Ninja
+
+rapids-print-env
+
+rapids-logger "Begin py build"
+
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+conda config --set path_conflict prevent
+
+sccache --zero-stats
+
+rapids-conda-retry mambabuild \
+  --channel "${CPP_CHANNEL}" \
+  conda/recipes/rapidsmp
+
+sccache --show-adv-stats
+
+rapids-upload-conda-to-s3 python
