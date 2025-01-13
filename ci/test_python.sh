b4b9e24b9fce8d009d8f603009bee5df5aafc2c1
Richard (Rick) Zamora <rzamora217@gmail.com>
2025-01-13 02:54:17 -0600

    Add placeholder Python package (#24)
    This PR is making a rough attempt to add a `rapidsmp` Python package to the project. Build/Cython is not my area of expertise, so I'm starting with a "placeholder" Python package.
    
    Contributes to https://github.com/rapidsai/rapids-multi-gpu/issues/22
    
    Authors:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/rapids-multi-gpu/pull/24

---
+++ ci/test_python.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+. /opt/conda/etc/profile.d/conda.sh
+
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
+
+rapids-logger "Generate Python testing dependencies"
+rapids-dependency-file-generator \
+  --output conda \
+  --file-key test_python \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" \
+  --prepend-channel "${CPP_CHANNEL}" --prepend-channel "${PYTHON_CHANNEL}" \
+  | tee env.yaml
+
+rapids-mamba-retry env create -qy -f env.yaml -n test
+
+# Temporarily allow unbound variables for conda activation.
+set +u
+conda activate test
+set -u
+
+RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
+mkdir -p "${RAPIDS_TESTS_DIR}"
+
+rapids-print-env
+
+rapids-logger "Check GPU usage"
+nvidia-smi
+
+# Support invoking test_python.sh outside the script directory
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
+
+rapids-logger "pytest rapidsmp"
+./ci/run_pytests.sh && EXITCODE=$? || EXITCODE=$?;
+
+rapids-logger "Test script exiting with value: $EXITCODE"
+exit ${EXITCODE}
