425e243690fe840a9d8c6ad69c3b0a88b974c4ac
Mads R. B. Kristensen <madsbk@gmail.com>
2025-01-14 19:46:49 +0100

    run_pytests.sh: fix test path

--- ci/run_pytests.sh
+++ ci/run_pytests.sh
@@ -4,6 +4,6 @@
 set -euo pipefail
 
 # Support invoking run_pytests.sh outside the script directory
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/rapidsmp
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/rapidsmp/rapidsmp
 
 pytest --cache-clear --verbose "$@" tests
