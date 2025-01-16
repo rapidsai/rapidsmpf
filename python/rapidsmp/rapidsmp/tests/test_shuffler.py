ea55de24fb3684d2f2bb504fb9ea70f067ed5134
Mads R. B. Kristensen <madsbk@gmail.com>
2025-01-16 10:02:19 +0100

    test_partition_and_pack_unpack: more parameters

--- python/rapidsmp/rapidsmp/tests/test_shuffler.py
+++ python/rapidsmp/rapidsmp/tests/test_shuffler.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 from __future__ import annotations
 
+import pytest
 from mpi4py import MPI
 
 import cudf
@@ -13,13 +14,15 @@ from rapidsmp.shuffler import Shuffler, partition_and_pack, unpack_and_concat
 from rapidsmp.testing import assert_eq, to_cudf_dataframe, to_pylibcudf_table
 
 
-def test_partition_and_pack_unpack():
-    expect = cudf.DataFrame({"0": [1, 2, 3], "1": [2, 2, 1]})
-
+@pytest.mark.parametrize("df", [{"0": [1, 2, 3], "1": [2, 2, 1]}, {"0": [], "1": []}])
+@pytest.mark.parametrize("num_partitions", [1, 2, 3, 10])
+def test_partition_and_pack_unpack(df, num_partitions):
+    expect = cudf.DataFrame(df)
     partitions = partition_and_pack(
-        to_pylibcudf_table(expect), columns_to_hash=(1,), num_partitions=2
+        to_pylibcudf_table(expect), columns_to_hash=(1,), num_partitions=num_partitions
     )
     got = to_cudf_dataframe(unpack_and_concat(tuple(partitions.values())))
+    # Since the row order isn't preserved, we sort before comparing.
     assert_eq(expect.sort_values(by="0"), got.sort_values(by="0"))
 
 
