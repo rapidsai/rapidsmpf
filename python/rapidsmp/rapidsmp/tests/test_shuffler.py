dd515d2d1c468498bcad52215ed96140cfa15513
rjzamora <rzamora217@gmail.com>
2025-01-17 11:43:19 -0800

    fix extract bug and update pytests

--- python/rapidsmp/rapidsmp/tests/test_shuffler.py
+++ python/rapidsmp/rapidsmp/tests/test_shuffler.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 from __future__ import annotations
 
+import math
+
+import numpy as np
 import pytest
 from mpi4py import MPI
 
@@ -67,3 +70,73 @@ def test_shuffler_single_nonempty_partition(total_num_partitions):
     )
     if not res.empty:
         assert_eq(res, df, sort_rows="0")
+
+
+@pytest.mark.parametrize("batch_size", [None, 10])
+@pytest.mark.parametrize("total_num_partitions", [1, 2, 3, 10])
+def test_shuffler_uniform(batch_size, total_num_partitions):
+    mpi_comm = MPI.COMM_WORLD
+    comm = new_communicator(mpi_comm)
+    br = BufferResource(rmm.mr.get_current_device_resource())
+
+    num_rows = 100
+    df = cudf.DataFrame(
+        {
+            "a": range(num_rows),
+            "b": np.random.randint(0, 1000, num_rows),
+            "c": ["cat", "dog"] * (num_rows // 2),
+        }
+    )
+    columns_to_hash = (df.columns.get_loc("b"),)
+    column_names = list(df.columns)
+
+    # Calculate exected output partitions on all ranks
+    expected = {
+        partition_id: pylibcudf_to_cudf_dataframe(
+            unpack_and_concat([packed]),
+            column_names=column_names,
+        )
+        for partition_id, packed in partition_and_pack(
+            cudf_to_pylibcudf_table(df),
+            columns_to_hash=columns_to_hash,
+            num_partitions=total_num_partitions,
+        ).items()
+    }
+
+    # Create shuffler
+    shuffler = Shuffler(
+        comm,
+        total_num_partitions=total_num_partitions,
+        stream=DEFAULT_STREAM,
+        br=br,
+    )
+
+    # Slice df and submit local slices to shuffler
+    stride = math.ceil(num_rows / mpi_comm.size)
+    local_df = df.iloc[comm.rank * stride : (comm.rank + 1) * stride]
+    num_rows_local = len(local_df)
+    batch_size = batch_size or num_rows_local
+    for i in range(0, num_rows_local, batch_size):
+        packed_inputs = partition_and_pack(
+            cudf_to_pylibcudf_table(local_df.iloc[i : i + batch_size]),
+            columns_to_hash=columns_to_hash,
+            num_partitions=total_num_partitions,
+        )
+        shuffler.insert_chunks(packed_inputs)
+
+    # Tell shuffler we are done adding data
+    for pid in range(total_num_partitions):
+        shuffler.insert_finished(pid)
+
+    # Extract and check shuffled partitions
+    while not shuffler.finished():
+        partition_id = shuffler.wait_any()
+        packed_chunks = shuffler.extract(partition_id)
+        partition = unpack_and_concat(packed_chunks)
+        assert_eq(
+            pylibcudf_to_cudf_dataframe(partition, column_names=column_names),
+            expected[partition_id],
+            sort_rows="a",
+        )
+
+    shuffler.shutdown()
