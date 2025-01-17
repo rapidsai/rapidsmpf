dcd42615342f3b4e86e1498c178193b3268bc734
Peter Andreas Entschev <peter@entschev.com>
2025-01-17 02:25:56 -0800

    Add timeouts to ctests

--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 set -xeuo pipefail
 
@@ -12,12 +12,12 @@ export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export OMPI_MCA_opal_cuda_support=1  # enable CUDA support in OpenMPI
 
 # Run gtests (single rank)
-ctest --no-tests=error --output-on-failure "$@"
+timeout 1m ctest --no-tests=error --output-on-failure "$@"
 
 # Run gtests with mpirun. Note, we run with many different number of ranks,
 # which we can do as long as the test suite only takes seconds to run.
-mpirun -np 2 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 3 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 4 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 5 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 8 ctest --no-tests=error --output-on-failure "$@"
+timeout 1m mpirun -np 2 ctest --no-tests=error --output-on-failure "$@"
+timeout 1m mpirun -np 3 ctest --no-tests=error --output-on-failure "$@"
+timeout 1m mpirun -np 4 ctest --no-tests=error --output-on-failure "$@"
+timeout 1m mpirun -np 5 ctest --no-tests=error --output-on-failure "$@"
+timeout 1m mpirun -np 8 ctest --no-tests=error --output-on-failure "$@"
