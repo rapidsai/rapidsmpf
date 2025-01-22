8f15b2f34136bd2351ff1fd30c5c439ac39e17c5
Mads R. B. Kristensen <madsbk@gmail.com>
2025-01-22 16:01:19 +0100

    Fix the python shuffler tests (#51)
    Also now running python tests with mpirun
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    URL: https://github.com/rapidsai/rapids-multi-gpu/pull/51

--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 set -xeuo pipefail
 
@@ -11,13 +11,17 @@ export OMPI_ALLOW_RUN_AS_ROOT=1  # CI runs as root
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export OMPI_MCA_opal_cuda_support=1  # enable CUDA support in OpenMPI
 
-# Run gtests (single rank)
-ctest --no-tests=error --output-on-failure "$@"
+EXTRA_ARGS="$@"
+run_mpirun_test() {
+    local timeout="$1" # Timeout
+    local nrank="$2"   # Number of ranks
+    echo "Running ctest with $nrank ranks"
+    timeout "$timeout" mpirun -np "$nrank" ctest --no-tests=error \
+        --output-on-failure $EXTRA_ARGS
+}
 
-# Run gtests with mpirun. Note, we run with many different number of ranks,
-# which we can do as long as the test suite only takes seconds to run.
-mpirun -np 2 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 3 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 4 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 5 ctest --no-tests=error --output-on-failure "$@"
-mpirun -np 8 ctest --no-tests=error --output-on-failure "$@"
+# Note, we run with many different number of ranks, which we can do as long as
+# the test suite only takes seconds to run (timeouts after one minute).
+for nrank in 1 2 3 4 5 8; do
+    run_mpirun_test 1m $nrank
+done
