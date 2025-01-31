95b96bd12b0373cf37caf8c870a34617f9b6bb1a
Mads R. B. Kristensen <madsbk@gmail.com>
2025-01-31 16:17:36 +0100

    python bindings for logger.warn

--- python/rapidsmp/rapidsmp/communicator/communicator.pyx
+++ python/rapidsmp/rapidsmp/communicator/communicator.pyx
@@ -1,6 +1,52 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 
 from cython.operator cimport dereference as deref
+from libcpp.memory cimport shared_ptr
+from libcpp.string cimport string
+from libcpp.utility cimport move
+
+
+# Since a rapids::Communicator::Logger doesn't have a default ctor, we use
+# this C++ function to call the logger instance without having Cython try
+# to use the non-existent default ctor.
+cdef extern from *:
+    """
+    template<typename T>
+    void cpp_log_warn(std::shared_ptr<rapidsmp::Communicator> comm, T && msg) {
+        comm->logger().warn(msg);
+    }
+    """
+    void cpp_log_warn[T](shared_ptr[cpp_Communicator] comm, T msg) except +
+
+
+cdef class Logger:
+    """
+    Logger.
+
+    To control the verbosity level, set the environment variable `RAPIDSMP_LOG`:
+      - `0`: Disable all logging.
+      - `1`: Enable warnings only.
+      - `2`: Enable warnings and informational messages (default).
+      - `3`: Enable warnings, informational, and debug messages.
+      - `4`: Enable warnings, informational, debug, and trace messages.
+    """
+
+    def __init__(self):
+        raise TypeError("Please get a `Logger` from a communicater instance")
+
+    def warn(self, msg: str):
+        """
+        Logs a warning message.
+
+        Formats and outputs a warning message if the verbosity level is `1` or higher.
+
+        Parameters
+        ----------
+        msg
+            The warning message to log.
+        """
+        cdef string _msg = msg.encode()
+        cpp_log_warn(self._comm._handle, move(_msg))
 
 
 cdef class Communicator:
@@ -25,6 +71,10 @@ cdef class Communicator:
             "`rapidsmp.mpi.new_communicator()`"
         )
 
+    def __cinit__(self):
+        self._logger = Logger.__new__(Logger)
+        self._logger._comm = self
+
     @property
     def rank(self):
         """
@@ -46,3 +96,14 @@ cdef class Communicator:
             Total number of ranks.
         """
         return deref(self._handle).nranks()
+
+    @property
+    def logger(self):
+        """
+        Get the logger.
+
+        Returns
+        -------
+            A logger instance.
+        """
+        return self._logger
--- python/rapidsmp/rapidsmp/tests/test_communicator.py
+++ python/rapidsmp/rapidsmp/tests/test_communicator.py
@@ -4,9 +4,14 @@ from __future__ import annotations
 from mpi4py import MPI
 
 from rapidsmp.communicator.mpi import new_communicator
+from rapidsmp.testing import capture_output
 
 
 def test_mpi():
     comm = new_communicator(MPI.COMM_WORLD)
     assert comm.nranks == MPI.COMM_WORLD.size
     assert comm.rank == MPI.COMM_WORLD.rank
+
+    with capture_output() as output:
+        comm.logger.warn("this is a warning")
+    assert "this is a warning" in output.read()
