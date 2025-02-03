030584b69716d5b0ce645c0b739864075f2c323e
Mads R. B. Kristensen <madsbk@gmail.com>
2025-02-03 14:32:34 +0100

    test different log levels

--- python/rapidsmp/rapidsmp/tests/test_communicator.py
+++ python/rapidsmp/rapidsmp/tests/test_communicator.py
@@ -1,20 +1,31 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 from __future__ import annotations
 
+import pytest
 from mpi4py import MPI
 
 from rapidsmp.communicator.mpi import new_communicator
 
 
-def test_mpi(capfd):
-    comm = new_communicator(MPI.COMM_WORLD)
-    assert comm.nranks == MPI.COMM_WORLD.size
-    assert comm.rank == MPI.COMM_WORLD.rank
+@pytest.mark.parametrize("RAPIDSMP_LOG", range(5))
+def test_mpi(capfd, RAPIDSMP_LOG):
+    with pytest.MonkeyPatch.context() as monkeypatch:
+        monkeypatch.setenv("RAPIDSMP_LOG", str(RAPIDSMP_LOG))
 
-    comm.logger.warn("WARN")
-    comm.logger.info("INFO")
-    comm.logger.debug("DEBUG")
-    comm.logger.trace("TRACE")
-    output = capfd.readouterr().out
+        comm = new_communicator(MPI.COMM_WORLD)
+        assert comm.nranks == MPI.COMM_WORLD.size
+        assert comm.rank == MPI.COMM_WORLD.rank
 
-    assert output.count("WARN") == 2
+        comm.logger.warn("WARN")
+        comm.logger.info("INFO")
+        comm.logger.debug("DEBUG")
+        comm.logger.trace("TRACE")
+        output = capfd.readouterr().out
+
+        # Iff the verbosity level is high enough for a log category, we expect the
+        # category name to be printed exactly twice. E.g., a warning would look like:
+        # "[WARN:0:0] WARN".
+        assert output.count("WARN") == (2 if RAPIDSMP_LOG > 0 else 0)
+        assert output.count("INFO") == (2 if RAPIDSMP_LOG > 1 else 0)
+        assert output.count("DEBUG") == (2 if RAPIDSMP_LOG > 2 else 0)
+        assert output.count("TRACE") == (2 if RAPIDSMP_LOG > 3 else 0)
