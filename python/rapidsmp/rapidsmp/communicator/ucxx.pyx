ed99b99e8ab1fce031d8f769043d7d8fa6750cd0
Peter Andreas Entschev <peter@entschev.com>
2025-02-20 05:39:38 -0800

    Fix linting

--- python/rapidsmp/rapidsmp/communicator/ucxx.pyx
+++ python/rapidsmp/rapidsmp/communicator/ucxx.pyx
@@ -115,8 +115,8 @@ def get_root_ucxx_address(Communicator comm):
     """
     Get the address of the communicator's UCXX worker.
 
-    This function is intended to be called from the root rank to communicate 
-    to other processes how to reach the root, but it will return the address of 
+    This function is intended to be called from the root rank to communicate
+    to other processes how to reach the root, but it will return the address of
     UCXX worker of other ranks too.
 
     Parameters
