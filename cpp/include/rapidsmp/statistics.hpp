d506b62360aa2d5eecf47010c6a6c7bfb184fc2f
Mads R. B. Kristensen <madsbk@gmail.com>
2025-02-21 13:28:30 +0100

    src/statistics.cpp

--- cpp/include/rapidsmp/statistics.hpp
+++ cpp/include/rapidsmp/statistics.hpp
@@ -15,11 +15,8 @@
  */
 #pragma once
 #include <cstddef>
-#include <iomanip>
 #include <mutex>
-#include <sstream>
 #include <string>
-#include <utility>
 
 #include <rapidsmp/buffer/buffer.hpp>
 #include <rapidsmp/communicator/communicator.hpp>
@@ -103,13 +100,7 @@ class Statistics {
      * @param peer The rank of the peer to retrieve statistics for.
      * @return A PeerStats object for the specified peer.
      */
-    PeerStats get_peer_stats(Rank peer) const {
-        if (!enabled()) {
-            return PeerStats{};
-        }
-        std::lock_guard<std::mutex> lock(mutex_);
-        return peer_stats_.at(peer);
-    }
+    PeerStats get_peer_stats(Rank peer) const;
 
     /**
      * @brief Add peer communication to the statistics.
@@ -120,15 +111,7 @@ class Statistics {
      * @param nbytes The number of bytes communicated.
      * @return The total number of bytes communicated to the peer after the update.
      */
-    std::size_t add_peer_comm(Rank peer, std::size_t nbytes) {
-        if (!enabled()) {
-            return 0;
-        }
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto& p = peer_stats_.at(peer);
-        ++p.comm_count;
-        return p.comm_nbytes += nbytes;
-    }
+    std::size_t add_peer_comm(Rank peer, std::size_t nbytes);
 
     /**
      * @brief Generates a report of statistics in a formatted string.
@@ -137,33 +120,7 @@ class Statistics {
      * @param label_width The width of the labels in the report.
      * @return A string representing the formatted statistics report.
      */
-    std::string report(int column_width = 12, int label_width = 22) const {
-        if (!enabled()) {
-            return "Statistics: disabled";
-        }
-        std::lock_guard<std::mutex> lock(mutex_);
-        std::stringstream ss;
-        ss << "Statistics:\n";
-        ss << std::setw(label_width - 3) << std::left << " - peers:";
-        for (Rank i = 0; i < comm_->nranks(); ++i) {
-            ss << std::right << std::setw(column_width) << "Rank" << i;
-        }
-        ss << "\n" << std::setw(label_width) << std::left << " - comm-gpu-data-total:";
-        for (Rank i = 0; i < comm_->nranks(); ++i) {
-            ss << std::right << std::setw(column_width)
-               << format_nbytes(peer_stats_.at(i).comm_nbytes) << " ";
-        }
-        ss << "\n" << std::setw(label_width) << std::left << " - comm-gpu-data-mean:";
-        for (Rank i = 0; i < comm_->nranks(); ++i) {
-            ss << std::right << std::setw(column_width)
-               << format_nbytes(
-                      peer_stats_.at(i).comm_nbytes / (double)peer_stats_.at(i).comm_count
-                  )
-               << " ";
-        }
-        ss << "\n";
-        return ss.str();
-    }
+    std::string report(int column_width = 12, int label_width = 22) const;
 
   private:
     mutable std::mutex mutex_;
---
+++ cpp/src/statistics.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iomanip>
+#include <sstream>
+
+#include <rapidsmp/statistics.hpp>
+
+namespace rapidsmp {
+
+Statistics::PeerStats Statistics::get_peer_stats(Rank peer) const {
+    if (!enabled()) {
+        return PeerStats{};
+    }
+    std::lock_guard<std::mutex> lock(mutex_);
+    return peer_stats_.at(peer);
+}
+
+std::size_t Statistics::add_peer_comm(Rank peer, std::size_t nbytes) {
+    if (!enabled()) {
+        return 0;
+    }
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto& p = peer_stats_.at(peer);
+    ++p.comm_count;
+    return p.comm_nbytes += nbytes;
+}
+
+std::string Statistics::report(int column_width, int label_width) const {
+    if (!enabled()) {
+        return "Statistics: disabled";
+    }
+    std::lock_guard<std::mutex> lock(mutex_);
+    std::stringstream ss;
+    ss << "Statistics:\n";
+    ss << std::setw(label_width - 3) << std::left << " - peers:";
+    for (Rank i = 0; i < comm_->nranks(); ++i) {
+        ss << std::right << std::setw(column_width) << "Rank" << i;
+    }
+    ss << "\n" << std::setw(label_width) << std::left << " - comm-gpu-data-total:";
+    for (Rank i = 0; i < comm_->nranks(); ++i) {
+        ss << std::right << std::setw(column_width)
+           << format_nbytes(peer_stats_.at(i).comm_nbytes) << " ";
+    }
+    ss << "\n" << std::setw(label_width) << std::left << " - comm-gpu-data-mean:";
+    for (Rank i = 0; i < comm_->nranks(); ++i) {
+        ss << std::right << std::setw(column_width)
+           << format_nbytes(
+                  peer_stats_.at(i).comm_nbytes / (double)peer_stats_.at(i).comm_count
+              )
+           << " ";
+    }
+    ss << "\n";
+    return ss.str();
+}
+}  // namespace rapidsmp
