fbf687049cda911403b8e2fe3d0653f243a64d34
rjzamora <rzamora217@gmail.com>
2025-02-24 13:36:18 -0800

    add dask_cudf_shuffle shuffle example

--- python/rapidsmp/rapidsmp/tests/test_dask.py
+++ python/rapidsmp/rapidsmp/tests/test_dask.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 from __future__ import annotations
 
+import dask
 import pytest
 from dask.distributed import Client
 from dask_cuda import LocalCUDACluster
 from dask_cuda.utils import get_n_gpus
-from distributed.utils_test import gen_test
+from distributed.utils_test import cleanup, gen_test, loop, loop_in_thread  # noqa: F401
 from mpi4py import MPI
 
 from rapidsmp.integrations.dask import rapidsmp_ucxx_comm_setup
@@ -39,3 +40,37 @@ async def test_dask_ucxx_cluster():
 
         result = await client.run(get_rank)
         assert set(result.values()) == set(range(len(cluster.workers)))
+
+
+@pytest.mark.parametrize("partition_count", [None, 3])
+def test_dask_cudf_integration(loop, partition_count):  # noqa: F811
+    # Test basic Dask-cuDF integration
+
+    pytest.importorskip("dask_cudf")
+
+    import dask.dataframe as dd
+
+    from rapidsmp.examples.dask import dask_cudf_shuffle
+    from rapidsmp.integrations.dask import LocalRMPCluster
+
+    with LocalRMPCluster(loop=loop) as cluster:  # noqa: SIM117
+        with Client(cluster):
+            df = (
+                dask.datasets.timeseries(
+                    freq="3600s",
+                    partition_freq="2D",
+                )
+                .reset_index(drop=True)
+                .to_backend("cudf")
+            )
+            partition_count_in = df.npartitions
+            expect = df.compute().sort_values(["x", "y"])
+            shuffled = dask_cudf_shuffle(
+                df,
+                ["name", "id"],
+                partition_count=partition_count,
+            )
+            assert shuffled.npartitions == (partition_count or partition_count_in)
+            got = shuffled.compute().sort_values(["x", "y"])
+
+            dd.assert_eq(expect, got, check_index=False)
