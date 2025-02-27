a4d43dfdab109f817f8ee185c9cd49a110166f53
Peter Andreas Entschev <peter@entschev.com>
2025-02-27 04:32:45 -0800

    Fix arch variable

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -36,7 +36,7 @@ nvidia-smi
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
 
 rapids-logger "pytest rapidsmp"
-if [[ "${arch}" == "aarch64" ]]; then
+if [[ "$(arch)" == "aarch64" ]]; then
   ./ci/run_pytests.sh --ignore=tests/test_ray.py && EXITCODE=$? || EXITCODE=$?;
 else
   ./ci/run_pytests.sh && EXITCODE=$? || EXITCODE=$?;
