e3ee319f9d73e5a1735b29483b73a2e435427761
Mads R. B. Kristensen <madsbk@gmail.com>
2025-02-28 16:02:31 +0100

    move to .cpp

--- cpp/include/rapidsmp/option.hpp
+++ cpp/include/rapidsmp/option.hpp
@@ -84,27 +84,6 @@ T getenv_or(std::string const& env_var_name, T default_val) {
  * interpreted as a boolean.
  */
 template <>
-inline bool getenv_or(std::string const& env_var_name, bool default_val) {
-    auto const* env_val = std::getenv(env_var_name.c_str());
-    if (env_val == nullptr) {
-        return default_val;
-    }
-    try {
-        // Try parsing `env_var_name` as a integer
-        return static_cast<bool>(std::stoi(env_val));
-    } catch (std::invalid_argument const&) {
-    }
-
-    std::string str = detail::trim_and_lowercase(env_val);
-    if (str == "true" || str == "on" || str == "yes") {
-        return true;
-    }
-    if (str == "false" || str == "off" || str == "no") {
-        return false;
-    }
-    throw std::invalid_argument(
-        "unknown config value " + std::string{env_var_name} + "=" + std::string{env_val}
-    );
-}
+bool getenv_or(std::string const& env_var_name, bool default_val);
 
 }  // namespace rapidsmp
--- cpp/src/option.cpp
+++ cpp/src/option.cpp
@@ -40,4 +40,30 @@ std::string trim_and_lowercase(std::string str) {
     return str;
 }
 }  // namespace detail
+
+template <>
+bool getenv_or(std::string const& env_var_name, bool default_val) {
+    auto const* env_val = std::getenv(env_var_name.c_str());
+    if (env_val == nullptr) {
+        return default_val;
+    }
+    try {
+        // Try parsing `env_var_name` as a integer
+        return static_cast<bool>(std::stoi(env_val));
+    } catch (std::invalid_argument const&) {
+    }
+
+    std::string str = detail::trim_and_lowercase(env_val);
+    if (str == "true" || str == "on" || str == "yes") {
+        return true;
+    }
+    if (str == "false" || str == "off" || str == "no") {
+        return false;
+    }
+    throw std::invalid_argument(
+        "unknown config value " + std::string{env_var_name} + "=" + std::string{env_val}
+    );
+}
+
+
 }  // namespace rapidsmp
