5c0cd56558c4963fc272bd21574dc668c06da3ab
Mads R. B. Kristensen <madsbk@gmail.com>
2025-03-03 09:08:11 +0100

    check stat name

--- cpp/tests/test_statistics.cpp
+++ cpp/tests/test_statistics.cpp
@@ -47,8 +47,10 @@ TEST(Statistics, Communication) {
     EXPECT_EQ(stats.add_stat("custom-formatter", 1, custom_formatter), 11);
     EXPECT_EQ(stats.get_stat("custom-formatter").count_, 2);
     EXPECT_EQ(stats.get_stat("custom-formatter").value_, 11);
+    EXPECT_THAT(stats.report(), ::testing::HasSubstr("custom-formatter"));
     EXPECT_THAT(stats.report(), ::testing::HasSubstr("11 by custom formatter"));
 
     EXPECT_EQ(stats.add_bytes_stat("byte-statistics", 20), 20);
+    EXPECT_THAT(stats.report(), ::testing::HasSubstr("byte-statistics"));
     EXPECT_THAT(stats.report(), ::testing::HasSubstr("20.00 B"));
 }
