e7fbdf40747c7868eda574d46dbc812b959b7198
Richard (Rick) Zamora <rzamora217@gmail.com>
2025-03-06 15:14:03 -0600

    Dask-integration API and Dask-cuDF example (#92)
    Depends on https://github.com/rapidsai/rapids-multi-gpu/pull/89 (**MERGED**)
    Closes #85 
    
    This PR introduces basic functionality for generating a Dask task graph for a `rapidsmp`-powered shuffle operation.
    
    **Important APIs**:
    
    - `rapidsmp.integrations.dask.LocalRMPCluster`: This is a thin wrapper for `LocalCUDACluster`. It's primary purpose is to preload a `RMPSchedulerPlugin` object on the scheduler when it is first initialized. This scheduler plugin is used to apply the worker restrictions needed to coordinate output tasks with specific `rapidsmp` ranks.
    - `rapidsmp_shuffle_graph`: This is a general utility for constructing a task graph for a hash-based shuffle operation. This API can be used by both Dask-cuDF and multi-GPU cuDF-Polars.
    - `rapidsmp.examples.dask.dask_cudf_shuffle`: This is a collection-level API for Dask-cuDF DataFrame shuffling. 
    
    TODO:
    - [x] Add dask-cudf dependency for CI.
    - [x] Add documentation on the shuffle algorithm.
    
    **cuDF-Polars Integration Notes**
    
    The `LocalRMPCluster`/`rapidsmp_shuffle_graph` APIs currently "work" with multi-GPU cuDF-Polars on an experimental development branch: https://github.com/rjzamora/cudf/tree/cudf-polars-multi-combined
    
    I you build [rapidsmp:dask-shuffle-poc](https://github.com/rjzamora/rapids-multi-gpu/tree/dask-shuffle-poc) with [cudf:cudf-polars-multi-combined](https://github.com/rjzamora/cudf/tree/cudf-polars-multi-combined). You can run a rapidsmp-backed TPCh benchmark. E.g.
    
    ```
    $ RAPIDSMP_LOG=0 CUDA_VISIBLE_DEVICES="0,1,2,3" python cudf/python/cudf_polars/cudf_polars/experimental/benchmarks/tpch.py 5 --path /datasets/rzamora/data/tpch-data/scale-300.0 -e dask-cuda --n-workers 4
    
    shape: (5, 2)
    ┌───────────┬───────────┐
    │ n_name    ┆ revenue   │
    │ ---       ┆ ---       │
    │ str       ┆ f64       │
    ╞═══════════╪═══════════╡
    │ INDIA     ┆ 1.5902e10 │
    │ VIETNAM   ┆ 1.5894e10 │
    │ INDONESIA ┆ 1.5861e10 │
    │ CHINA     ┆ 1.5845e10 │
    │ JAPAN     ┆ 1.5793e10 │
    └───────────┴───────────┘
    ```
    
    **Task-graph Details**
    
    As illustrated in the figure below, `rapidsmp_shuffle_graph` currently produces a task graph containing two  **global**-barrier tasks, and one **worker**-barrier task per Dask worker. The global-barrier tasks can run on any Dask worker in the cluster. However, both the  **worker**-barrier tasks and partition-extraction tasks must be restricted to specific workers.
    
    ![Dask-Shuffle-Service-Double-Barrier drawio](https://github.com/user-attachments/assets/770e7c3c-13c7-4896-bf51-9e26664a2e28)
    
    Authors:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    Approvers:
      - Tom Augspurger (https://github.com/TomAugspurger)
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/rapids-multi-gpu/pull/92

---
+++ python/rapidsmp/rapidsmp/examples/dask.py
@@ -0,0 +1,155 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+"""Dask-cuDF integration."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import dask.dataframe as dd
+from dask.tokenize import tokenize
+
+import rmm.mr
+from rmm.pylibrmm.stream import DEFAULT_STREAM
+
+from rapidsmp.integrations.dask import rapidsmp_shuffle_graph
+from rapidsmp.shuffler import partition_and_pack, unpack_and_concat
+from rapidsmp.testing import pylibcudf_to_cudf_dataframe
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    import dask_cudf
+
+    import cudf
+
+    from rapidsmp.shuffler import Shuffler
+
+
+class DaskCudfIntegration:
+    """
+    Dask-cuDF protocol for Dask integration.
+
+    This protocol can be used to implement a rapidsmp-shuffle
+    operation on a Dask-cuDF collection.
+
+    See Also
+    --------
+    rapidsmp.integrations.dask.DaskIntegration
+        Base Dask-integration protocol definition.
+    """
+
+    @staticmethod
+    def insert_partition(
+        df: cudf.DataFrame,
+        on: Sequence[str],
+        partition_count: int,
+        shuffler: Shuffler,
+    ) -> None:
+        """
+        Add cudf DataFrame chunks to an RMP shuffler.
+
+        Parameters
+        ----------
+        df
+            DataFrame partition to add to a rapidsmp shuffler.
+        on
+            Sequence of column names to shuffle on.
+        partition_count
+            Number of output partitions for the current shuffle.
+        shuffler
+            The rapidsmp Shuffler object to extract from.
+        """
+        columns_to_hash = tuple(list(df.columns).index(val) for val in on)
+        packed_inputs = partition_and_pack(
+            df.to_pylibcudf()[0],
+            columns_to_hash=columns_to_hash,
+            num_partitions=partition_count,
+            stream=DEFAULT_STREAM,
+            device_mr=rmm.mr.get_current_device_resource(),
+        )
+        shuffler.insert_chunks(packed_inputs)
+
+    @staticmethod
+    def extract_partition(
+        partition_id: int,
+        column_names: list[str],
+        shuffler: Shuffler,
+    ) -> cudf.DataFrame:
+        """
+        Extract a finished partition from the RMP shuffler.
+
+        Parameters
+        ----------
+        partition_id
+            Partition id to extract.
+        column_names
+            Sequence of output column names.
+        shuffler
+            The rapidsmp Shuffler object to extract from.
+
+        Returns
+        -------
+        A shuffled DataFrame partition.
+        """
+        shuffler.wait_on(partition_id)
+        table = unpack_and_concat(
+            shuffler.extract(partition_id),
+            stream=DEFAULT_STREAM,
+            device_mr=rmm.mr.get_current_device_resource(),
+        )
+        return pylibcudf_to_cudf_dataframe(
+            table,
+            column_names=column_names,
+        )
+
+
+def dask_cudf_shuffle(
+    df: dask_cudf.DataFrame,
+    shuffle_on: list[str],
+    *,
+    partition_count: int | None = None,
+) -> dask_cudf.DataFrame:
+    """
+    Shuffle a dask_cudf.DataFrame with rapidsmp.
+
+    Parameters
+    ----------
+    df
+        Input `dask_cudf.DataFrame` collection.
+    shuffle_on
+        List of column names to shuffle on.
+    partition_count
+        Output partition count. Default will preserve
+        the input partition count.
+
+    Returns
+    -------
+    Shuffled Dask-cuDF DataFrame collection.
+    """
+    df0 = df.optimize()
+    count_in = df0.npartitions
+    count_out = partition_count or count_in
+    token = tokenize(df0, shuffle_on, count_out)
+    name_in = df0._name
+    name_out = f"shuffle-{token}"
+    graph = rapidsmp_shuffle_graph(
+        name_in,
+        name_out,
+        list(df0.columns),
+        shuffle_on,
+        count_in,
+        count_out,
+        DaskCudfIntegration,
+    )
+
+    # Add df0 dependencies to the task graph
+    graph.update(df0.dask)
+
+    # Return a Dask-DataFrame collection
+    return dd.from_graph(
+        graph,
+        df0._meta,
+        (None,) * (count_out + 1),
+        [(name_out, pid) for pid in range(count_out)],
+        "rapidsmp",
+    )
