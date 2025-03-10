652cd89dbc2f921e209e8f71304dfb71cc89b50e
Peter Andreas Entschev <peter@entschev.com>
2025-03-10 14:23:57 -0700

    Add `RAPIDSMP_CUDA_TRY`

--- cpp/include/rapidsmp/error.hpp
+++ cpp/include/rapidsmp/error.hpp
@@ -20,6 +20,76 @@
 
 namespace rapidsmp {
 
+/**
+ * @brief Exception thrown when a CUDA error is encountered.
+ *
+ * @ingroup errors
+ *
+ */
+struct cuda_error : public std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+/**
+ * @brief Exception thrown when an RAPIDSMP allocation fails
+ *
+ * @ingroup errors
+ *
+ */
+class bad_alloc : public std::bad_alloc {
+  public:
+    /**
+     * @brief Constructs a bad_alloc with the error message.
+     *
+     * @param msg Message to be associated with the exception
+     */
+    bad_alloc(const char* msg)
+        : _what{std::string{std::bad_alloc::what()} + ": " + msg} {}
+
+    /**
+     * @brief Constructs a bad_alloc with the error message.
+     *
+     * @param msg Message to be associated with the exception
+     */
+    bad_alloc(std::string const& msg) : bad_alloc{msg.c_str()} {}
+
+    /**
+     * @brief Returns the explanatory string.
+     *
+     * @return The explanatory string.
+     */
+    [[nodiscard]] const char* what() const noexcept override {
+        return _what.c_str();
+    }
+
+  private:
+    std::string _what;
+};
+
+/**
+ * @brief Exception thrown when RAPIDSMP runs out of memory
+ *
+ * @ingroup errors
+ *
+ * This error should only be thrown when we know for sure a resource is out of memory.
+ */
+class out_of_memory : public bad_alloc {
+  public:
+    /**
+     * @brief Constructs an out_of_memory with the error message.
+     *
+     * @param msg Message to be associated with the exception
+     */
+    out_of_memory(const char* msg) : bad_alloc{std::string{"out_of_memory: "} + msg} {}
+
+    /**
+     * @brief Constructs an out_of_memory with the error message.
+     *
+     * @param msg Message to be associated with the exception
+     */
+    out_of_memory(std::string const& msg) : out_of_memory{msg.c_str()} {}
+};
+
 // Stringify a macro argument
 #define RAPIDSMP_STRINGIFY_DETAIL(x) #x
 #define RAPIDSMP_STRINGIFY(x) RAPIDSMP_STRINGIFY_DETAIL(x)
@@ -101,5 +171,100 @@ namespace rapidsmp {
     }
 
 #define RAPIDSMP_FAIL_1(_what) RAPIDSMP_FAIL_2(_what, std::logic_error)
+
+/**
+ * @brief Error checking macro for CUDA runtime API functions.
+ *
+ * Invokes a CUDA runtime API function call. If the call does not return
+ * `cudaSuccess`, invokes cudaGetLastError() to clear the error and throws an
+ * exception detailing the CUDA error that occurred
+ *
+ * Defaults to throwing rapidsmp::cuda_error, but a custom exception may also be
+ * specified.
+ *
+ * Example:
+ * ```c++
+ *
+ * // Throws rapidsmp::cuda_error if `cudaMalloc` fails
+ * RAPIDSMP_CUDA_TRY(cudaMalloc(&p, 100));
+ *
+ * // Throws std::runtime_error if `cudaMalloc` fails
+ * RAPIDSMP_CUDA_TRY(cudaMalloc(&p, 100), std::runtime_error);
+ * ```
+ *
+ */
+#define RAPIDSMP_CUDA_TRY(...)                                                         \
+    GET_RAPIDSMP_CUDA_TRY_MACRO(__VA_ARGS__, RAPIDSMP_CUDA_TRY_2, RAPIDSMP_CUDA_TRY_1) \
+    (__VA_ARGS__)
+#define GET_RAPIDSMP_CUDA_TRY_MACRO(_1, _2, NAME, ...) NAME
+#define RAPIDSMP_CUDA_TRY_2(_call, _exception_type)                                   \
+    do {                                                                              \
+        cudaError_t const error = (_call);                                            \
+        if (cudaSuccess != error) {                                                   \
+            cudaGetLastError();                                                       \
+            /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/                            \
+            throw _exception_type{                                                    \
+                std::string{"CUDA error at: "} + __FILE__ + ":"                       \
+                + RAPIDSMP_STRINGIFY(__LINE__) + ": " + cudaGetErrorName(error) + " " \
+                + cudaGetErrorString(error)                                           \
+            };                                                                        \
+        }                                                                             \
+    } while (0)
+#define RAPIDSMP_CUDA_TRY_1(_call) RAPIDSMP_CUDA_TRY_2(_call, rapidsmp::cuda_error)
+
+/**
+ * @brief Error checking macro for CUDA memory allocation calls.
+ *
+ * Invokes a CUDA memory allocation function call. If the call does not return
+ * `cudaSuccess`, invokes cudaGetLastError() to clear the error and throws an
+ * exception detailing the CUDA error that occurred
+ *
+ * Defaults to throwing rapidsmp::bad_alloc, but when `cudaErrorMemoryAllocation` is
+ * returned, rapidsmp::out_of_memory is thrown instead.
+ *
+ * Can be called with either 1 or 2 arguments:
+ * - RAPIDSMP_CUDA_TRY_ALLOC(cuda_call): Performs error checking without specifying bytes
+ * - RAPIDSMP_CUDA_TRY_ALLOC(cuda_call, num_bytes): Includes the number of bytes in the
+ * error message
+ */
+#define RAPIDSMP_CUDA_TRY_ALLOC(...)                                      \
+    GET_RAPIDSMP_CUDA_TRY_ALLOC_MACRO(                                    \
+        __VA_ARGS__, RAPIDSMP_CUDA_TRY_ALLOC_2, RAPIDSMP_CUDA_TRY_ALLOC_1 \
+    )                                                                     \
+    (__VA_ARGS__)
+#define GET_RAPIDSMP_CUDA_TRY_ALLOC_MACRO(_1, _2, NAME, ...) NAME
+
+#define RAPIDSMP_CUDA_TRY_ALLOC_2(_call, num_bytes)                                  \
+    do {                                                                             \
+        cudaError_t const error = (_call);                                           \
+        if (cudaSuccess != error) {                                                  \
+            cudaGetLastError();                                                      \
+            auto const msg = std::string{"CUDA error (failed to allocate "}          \
+                             + std::to_string(num_bytes) + " bytes) at: " + __FILE__ \
+                             + ":" + RAPIDSMP_STRINGIFY(__LINE__) + ": "             \
+                             + cudaGetErrorName(error) + " "                         \
+                             + cudaGetErrorString(error);                            \
+            if (cudaErrorMemoryAllocation == error) {                                \
+                throw rapidsmp::out_of_memory{msg};                                  \
+            }                                                                        \
+            throw rapidsmp::bad_alloc{msg};                                          \
+        }                                                                            \
+    } while (0)
+
+#define RAPIDSMP_CUDA_TRY_ALLOC_1(_call)                                     \
+    do {                                                                     \
+        cudaError_t const error = (_call);                                   \
+        if (cudaSuccess != error) {                                          \
+            cudaGetLastError();                                              \
+            auto const msg = std::string{"CUDA error at: "} + __FILE__ + ":" \
+                             + RAPIDSMP_STRINGIFY(__LINE__) + ": "           \
+                             + cudaGetErrorName(error) + " "                 \
+                             + cudaGetErrorString(error);                    \
+            if (cudaErrorMemoryAllocation == error) {                        \
+                throw rapidsmp::out_of_memory{msg};                          \
+            }                                                                \
+            throw rapidsmp::bad_alloc{msg};                                  \
+        }                                                                    \
+    } while (0)
 
 }  // namespace rapidsmp
