a50ca6f28a1522dd76aaef011e44a3083fa3ca2e
Peter Andreas Entschev <peter@entschev.com>
2025-03-14 12:25:45 -0700

    Add `ProgressThread`

---
+++ cpp/include/rapidsmp/progress_thread.hpp
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <list>
+#include <mutex>
+#include <thread>
+
+#include <rapidsmp/communicator/communicator.hpp>
+#include <rapidsmp/pausable_thread_loop.hpp>
+
+namespace rapidsmp {
+
+/**
+ * @brief The progress state of a function, can be either `InProgress` or `Done`.
+ */
+enum ProgressState : bool {
+    InProgress,
+    Done,
+};
+
+class ProgressThread;
+
+/**
+ * @typedef FunctionID
+ * @brief The unique ID of a function registered with `ProgressThread`.
+ */
+using FunctionID = std::pair<ProgressThread*, std::uint64_t>;
+
+/**
+ * @typedef Function
+ * @brief The function type supported by `ProgressThread`, returning the progress
+ * state of thee function.
+ */
+using Function = std::function<ProgressState()>;
+
+/**
+ * @brief Store state of a function.
+ */
+class FunctionState {
+  public:
+    FunctionState() = delete;
+
+    /**
+     * @brief Construct state of a function.
+     *
+     * @param function The function to execute.
+     * @param function_id The ID that was assigned to the function by `ProgressThread`.
+     */
+    FunctionState(Function function, FunctionID function_id);
+
+    /**
+     * @brief Execute the function.
+     *
+     * @return The progress state of the function, that can be either
+     * `ProgressState::InProgress` or `ProgressState::Done`.
+     */
+    ProgressState operator()();
+
+    Function function;  ///< The function to execute.
+    FunctionID
+        function_id;  ///< The unique identifier of the function this object refers to.
+    ProgressState latest_state{InProgress};  ///< Latest progress state of iterable.
+};
+
+/**
+ * @brief Equality operator for a `FunctionState`.
+ *
+ * @param lhs The left hand side of the operator.
+ * @param rhs The right hand side of the operator.
+ *
+ * @return Whether the objects point to the same function.
+ */
+constexpr bool operator==(const FunctionState& lhs, const FunctionState& rhs);
+
+/**
+ * @brief Equality operator for a `FunctionState`.
+ *
+ * @param lhs The left hand side of the operator.
+ * @param function_id The function ID to compare to.
+ *
+ * @return Whether the objects point to the same function.
+ */
+constexpr bool operator==(const FunctionState& lhs, const FunctionID& function_id);
+
+/**
+ * @brief A progress thread that can execute arbitrary functions.
+ *
+ * Execute each of the registered arbitrary functions in a separate thread. The
+ * functions are executed in the order they were registered, and a newly registered
+ * function will only execute for the first time in the next iteration of the
+ * progress thread.
+ */
+class ProgressThread {
+  public:
+    /**
+     * @brief Construct a new progress thread that can handle multiple functions.
+     *
+     * @param logger The logger to use.
+     */
+    ProgressThread(Communicator::Logger& logger);
+
+    ~ProgressThread();
+
+    /**
+     * @brief Shutdown the thread, blocking until all functions are done.
+     *
+     * @throw std::logic_error If the thread is already inactive.
+     */
+    void shutdown();
+
+    /**
+     * @brief Insert an iterable object to process as part of the event loop.
+     *
+     * @param function The function to register.
+     *
+     * @return The unique ID of the function that was registered.
+     */
+    FunctionID add_function(std::function<ProgressState()> function);
+
+    /**
+     * @brief Remove a function and stop processing it as part of the event loop.
+     *
+     * @param function_id The unique function ID returned by `add_function`.
+     *
+     * @throws std::logic_error if the function was not registered with this
+     * `ProgressThread` or was already removed.
+     */
+    void remove_function(FunctionID function_id);
+
+  private:
+    /**
+     * @brief The event loop progressing each of the iterables.
+     *
+     * The event loop continuously progresses registered iterables in no
+     * specific order.
+     *
+     * @param self The `ProgressThread` instance.
+     */
+    static void event_loop(ProgressThread* self);
+
+    bool active_{true};
+    detail::PausableThreadLoop thread_;
+    std::list<FunctionState> functions_;
+    std::thread event_loop_thread_;
+    std::atomic<bool> event_loop_thread_run_{true};
+    std::mutex mutex_;
+    std::uint64_t next_function_id_;
+    Communicator::Logger& logger_;
+};
+
+}  // namespace rapidsmp
---
+++ cpp/src/progress_thread.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <utility>
+
+#include <rapidsmp/error.hpp>
+#include <rapidsmp/progress_thread.hpp>
+#include <rapidsmp/utils.hpp>
+
+#include "rapidsmp/communicator/communicator.hpp"
+
+namespace rapidsmp {
+
+FunctionState::FunctionState(Function function, FunctionID function_id)
+    : function(std::move(function)), function_id{std::move(function_id)} {}
+
+ProgressState FunctionState::operator()() {
+    latest_state = function();
+    return latest_state;
+}
+
+constexpr bool operator==(const FunctionState& lhs, const FunctionState& rhs) {
+    return lhs.function_id == rhs.function_id;
+}
+
+constexpr bool operator==(const FunctionState& lhs, const FunctionID& function_id) {
+    return lhs.function_id == function_id;
+}
+
+ProgressThread::ProgressThread(Communicator::Logger& logger)
+    : thread_([this]() {
+          // This thread needs to have a cuda context associated with it.
+          // For now, do so by calling cudaFree to initialise the driver.
+          RAPIDSMP_CUDA_TRY(cudaFree(nullptr));
+
+          return event_loop(this);
+      }),
+      logger_(logger) {
+    thread_.resume();
+}
+
+ProgressThread::~ProgressThread() {
+    if (active_) {
+        shutdown();
+    }
+}
+
+void ProgressThread::shutdown() {
+    RAPIDSMP_EXPECTS(active_, "ProgressThread is inactive");
+    logger_.debug("ProgressThread.shutdown() - initiate");
+    event_loop_thread_run_.store(false);
+    thread_.stop();
+    logger_.debug("ProgressThread.shutdown() - done");
+    active_ = false;
+}
+
+FunctionID ProgressThread::add_function(std::function<ProgressState()> function) {
+    std::lock_guard const lock(mutex_);
+    auto id = std::make_pair<ProgressThread*, std::uint64_t>(this, next_function_id_++);
+    functions_.emplace_back(function, id);
+    return id;
+}
+
+void ProgressThread::remove_function(FunctionID function_id) {
+    decltype(functions_)::iterator state;
+    bool is_valid = false;
+
+    RAPIDSMP_EXPECTS(
+        function_id.first == this, "Function was not registered with this ProgressThread"
+    );
+
+    while (true) {
+        std::lock_guard const lock(mutex_);
+
+        if (!is_valid) {
+            // Only search for iterator on first attempt
+            state = std::find(functions_.begin(), functions_.end(), function_id);
+            RAPIDSMP_EXPECTS(
+                state != functions_.end(), "Iterable not registered or already removed"
+            );
+        }
+
+        if (state->latest_state == ProgressState::Done) {
+            functions_.erase(state);
+            return;
+        }
+    }
+}
+
+void ProgressThread::event_loop(ProgressThread* self) {
+    self->logger_.debug("event loop - start: ", self);
+
+    // Continue the loop until both the "run" flag is false and all
+    // ongoing communication is done.
+    // auto const t0_event_loop = Clock::now();
+    if (self->event_loop_thread_run_ || !self->functions_.empty()) {
+        {
+            std::lock_guard const lock(self->mutex_);
+            for (auto& function : self->functions_) {
+                if (function.latest_state == ProgressState::InProgress)
+                    function();
+            }
+        }
+    }
+    // self->statistics_->add_duration_stat(
+    //     "event-loop-total", Clock::now() - t0_event_loop
+    // );
+    self->logger_.debug("event loop - shutdown: ", self);
+}
+
+}  // namespace rapidsmp
