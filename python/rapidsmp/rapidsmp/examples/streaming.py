bf6129cf21ed64bfa41e9638132780e4fa9772c8
niranda perera <niranda.perera@gmail.com>
2025-03-17 16:32:13 -0700

    WIP

--- python/rapidsmp/rapidsmp/examples/streaming.py
+++ python/rapidsmp/rapidsmp/examples/streaming.py
@@ -7,14 +7,21 @@ import argparse
 
 import cupy as cp
 
+from mpi4py import MPI
+
 import cudf
 import rmm.mr
 from rmm.pylibrmm.stream import DEFAULT_STREAM
 
+import rapidsmp.communicator.mpi
+from rapidsmp.communicator import Communicator
 from rapidsmp.buffer.resource import BufferResource
+from rapidsmp.buffer.buffer import MemoryType
+from rapidsmp.buffer.resource import BufferResource, LimitAvailableMemory
 from rapidsmp.communicator.ucxx import new_communicator
 from rapidsmp.shuffler import Shuffler, partition_and_pack, unpack_and_concat
 from rapidsmp.statistics import Statistics
+from rapidsmp.utils.string import parse_bytes, format_bytes
 
 
 def generate_partition(n_rows: int = 100) -> cudf.DataFrame:
@@ -44,12 +51,52 @@ def parse_args(
 ) -> argparse.Namespace:  # numpydoc ignore=PR01,RT01
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(description=__doc__)
+
     parser.add_argument(
-        "--n-partitions", type=int, default=10, help="Number of partitions"
+        "--out-parts",
+        type=int | None,
+        help="Number of output partitions. Default: n_ranks in the cluster",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--local-sz", type=parse_bytes, default="1GiB", help="Local data size"
+    )
+
+    parser.add_argument(
+        "--part-sz",
+        type=parse_bytes,
+        default="1GiB",
+        help="Partition size. Local size will be split into partitions of this size",
     )
     parser.add_argument(
-        "--n-rows", type=int, default=100, help="Number of rows per partition"
+        "--comm",
+        type=str,
+        default="mpi",
+        help="Communicator type",
+        choices={"mpi", "ucx"},
     )
+
+    parser.add_argument(
+        "--rmm-pool-size",
+        type=parse_bytes,
+        default=format_bytes(int(rmm.mr.available_device_memory()[1] * 0.8)),
+        help=(
+            "The size of the RMM pool as a string with unit such as '2MiB' and '4KiB'. "
+            "Default to 80%% of the total device memory, which is %(default)s."
+        ),
+    )
+
+    parser.add_argument(
+        "--spill-device",
+        type=lambda x: None if x is None else parse_bytes(x),
+        default=None,
+        help=(
+            "Spilling device-to-host threshold as a string with unit such as '2MiB' "
+            "and '4KiB'. Default is no spilling"
+        ),
+    )
+
     parser.add_argument(
         "--report",
         action=argparse.BooleanOptionalAction,
@@ -65,70 +112,91 @@ def parse_args(
     return parser.parse_args(args)
 
 
-def main(args: list[str] | None = None) -> None:  # numpydoc ignore=PR01
-    """Example performing a streaming shuffle."""
-    parsed = parse_args(args)
+def ucxx_mpi_setup() -> Communicator:
+    """
+    Bootstrap UCXX cluster using MPI.
 
-    n_partitions = parsed.n_partitions
-    n_rows = parsed.n_rows
-    operation_id = 0
+    Returns
+    -------
+    Communicator
+        A new ucxx communicator.
+    """
+    import ucxx._lib.libucxx as ucx_api
 
-    comm = new_communicator(nranks=1, ucx_worker=None, root_ucxx_address=None)
+    from rapidsmp.communicator.ucxx import (
+        barrier,
+        get_root_ucxx_address,
+        new_communicator,
+    )
+
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        comm = new_communicator(MPI.COMM_WORLD.size, None, None)
+        root_address_str = get_root_ucxx_address(comm)
+    else:
+        root_address_str = None
+
+    root_address_str = MPI.COMM_WORLD.bcast(root_address_str, root=0)
+
+    if MPI.COMM_WORLD.Get_rank() != 0:
+        root_address = ucx_api.UCXAddress.create_from_buffer(root_address_str)
+        comm = new_communicator(MPI.COMM_WORLD.size, None, root_address)
+
+    assert comm.nranks == MPI.COMM_WORLD.size
+    barrier(comm)
+    return comm
+
+
+def setup_and_run(args: argparse.Namespace) -> None:
+    """
+    Setup the args.
+
+    Parameters
+    ----------
+    args
+    """
+    if args.cluster_type == "mpi":
+        comm = rapidsmp.communicator.mpi.new_communicator(MPI.COMM_WORLD)
+    elif args.cluster_type == "ucxx":
+        comm = ucxx_mpi_setup()
 
     # Create a RMM stack with both a device pool and statistics.
     mr = rmm.mr.StatisticsResourceAdaptor(
         rmm.mr.PoolMemoryResource(
             rmm.mr.CudaMemoryResource(),
+            initial_pool_size=args.rmm_pool_size,
+            maximum_pool_size=args.rmm_pool_size,
         )
     )
     rmm.mr.set_current_device_resource(mr)
-    br = BufferResource(mr)
-    statistics = Statistics(enable=True)
-    stream = DEFAULT_STREAM
-    shuffler = Shuffler(
-        comm=comm,
-        op_id=operation_id,
-        total_num_partitions=n_partitions,
-        stream=stream,
-        br=br,
-        statistics=statistics,
+
+    # Create a buffer resource that limits device memory if `--spill-device`
+    # is not None.
+    memory_available = (
+        None
+        if args.spill_device is None
+        else {MemoryType.DEVICE: LimitAvailableMemory(mr, limit=args.spill_device)}
     )
+    br = BufferResource(mr, memory_available)
 
-    for partition_id in range(n_partitions):
-        if parsed.progress:
-            print(f"Inserting partition {partition_id}", end="\r")
-        df = generate_partition(n_rows)
-        (table, _) = df.to_pylibcudf()
-        packed_inputs = partition_and_pack(
-            table,
-            columns_to_hash=[1],
-            num_partitions=n_partitions,
-            stream=stream,
-            device_mr=rmm.mr.get_current_device_resource(),
-        )
-        shuffler.insert_chunks(packed_inputs)
+    stats = Statistics(args.statistics)
 
-    for partition_id in range(n_partitions):
-        shuffler.insert_finished(partition_id)
+    MPI.COMM_WORLD.barrier()
+    start_time = MPI.Wtime()
+    streaming_shuffle(
+        comm=comm,
+        br=br,
+        statistics=stats,
+    )
+    elapsed_time = MPI.Wtime() - start_time
+    MPI.COMM_WORLD.barrier()
 
-    if parsed.progress:
-        print("\nShuffling...", flush=True)
 
-    while not shuffler.finished():
-        partition_id = shuffler.wait_any()
-        unpack_and_concat(
-            shuffler.extract(partition_id),
-            stream=DEFAULT_STREAM,
-            device_mr=rmm.mr.get_current_device_resource(),
-        )
-        if parsed.progress:
-            print(f"Finished partition {partition_id}", end="\r")
 
-    if parsed.progress:
-        print("\nDone!", flush=True)
-    if parsed.report:
-        print(statistics.report())
+def main(args: list[str] | None = None) -> None:  # numpydoc ignore=PR01
+    """Example performing a streaming shuffle."""
+    parsed = parse_args(args)
 
+    setup_and_run(parsed)
 
 if __name__ == "__main__":
     main()
