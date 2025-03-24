39519ed54a86171499b960e5c844ea24eecfd4d6
Mads R. B. Kristensen <madsbk@gmail.com>
2025-03-24 14:40:52 +0100

    doc

--- cpp/src/shuffler/shuffler.cpp
+++ cpp/src/shuffler/shuffler.cpp
@@ -243,7 +243,7 @@ void Shuffler::insert(std::unordered_map<PartID, cudf::packed_columns>&& chunks)
     RAPIDSMP_NVTX_FUNC_RANGE();
     auto& log = comm_->logger();
 
-    // Spill if current device memory usage is negative.
+    // Spill if current available device memory is negative.
     br_->spill_manager().spill_to_make_headroom(0);
 
     // Insert each chunk into the inbox.
