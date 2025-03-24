d2d8d861df9cff2aeae7a1554480cfa7720990ad
Mads R. B. Kristensen <madsbk@gmail.com>
2025-03-24 11:11:15 +0100

    move to spill_manager.{hpp,cpp}

--- cpp/include/rapidsmp/buffer/resource.hpp
+++ cpp/include/rapidsmp/buffer/resource.hpp
@@ -14,8 +14,8 @@
 #include <rmm/mr/device/statistics_resource_adaptor.hpp>
 
 #include <rapidsmp/buffer/buffer.hpp>
+#include <rapidsmp/buffer/spill_manager.hpp>
 #include <rapidsmp/error.hpp>
-#include <rapidsmp/pausable_thread_loop.hpp>
 #include <rapidsmp/utils.hpp>
 
 namespace rapidsmp {
@@ -90,108 +90,6 @@ class MemoryReservation {
     MemoryType mem_type_;  ///< The type of memory for this reservation.
     BufferResource* br_;  ///< The buffer resource that manages this reservation.
     std::size_t size_;  ///< The remaining size of the reserved memory in bytes.
-};
-
-/**
- * @brief Manages memory spilling to free up device memory when needed.
- *
- * The SpillManager is responsible for registering, prioritizing, and executing spill
- * functions to ensure efficient memory management.
- */
-class SpillManager {
-  public:
-    /**
-     * @brief Spill function type.
-     *
-     * A spill function takes a requested spill amount as input and returns the actual
-     * amount of memory (in bytes) that was spilled.
-     */
-    using SpillFunction = std::function<std::size_t(std::size_t)>;
-
-    /**
-     * @brief Represents a unique identifier for a registered spill function.
-     */
-    using SpillFunctionID = std::size_t;
-
-    /**
-     * @brief Constructs a SpillManager instance.
-     *
-     * @param br Buffer resource used to retrieve current available memory.
-     * @param periodic_spill_check Optional time interval for periodic spill checks. If
-     * `std::nullopt`, no periodic spill check is performed.
-     */
-    SpillManager(
-        BufferResource* br,
-        std::optional<std::chrono::microseconds> periodic_spill_check = std::nullopt
-    );
-
-    /**
-     * @brief Destructor for SpillManager.
-     *
-     * Cleans up any allocated resources and stops periodic spill checks if active (this
-     * will block until all spill functions has stopped).
-     */
-    ~SpillManager();
-
-    /**
-     * @brief Adds a spill function with a given priority to the spill manager.
-     *
-     * The spill function is prioritized according to the specified priority value.
-     *
-     * @param spill_function The spill function to be added.
-     * @param priority The priority level of the spill function (higher values indicate
-     * higher priority).
-     * @return The id assigned to the newly added spill function.
-     */
-    SpillFunctionID add_spill_function(SpillFunction spill_function, int priority);
-
-    /**
-     * @brief Removes a spill function from the spill manager.
-     *
-     * This method unregisters the spill function associated with the given ID and removes
-     * it from the priority list. If no more spill functions remain, the periodic spill
-     * thread is paused.
-     *
-     * @param fid The id of the spill function to be removed.
-     */
-    void remove_spill_function(SpillFunctionID fid);
-
-    /**
-     * @brief Initiates spilling to free up a specified amount of memory.
-     *
-     * This method iterates through registered spill functions in priority order, invoking
-     * them until at least the requested amount of memory has been spilled or no more
-     * spilling is possible.
-     *
-     * @param amount The amount of memory (in bytes) to spill.
-     * @return The actual amount of memory spilled (in bytes), which may be more, less
-     * or equal to the requested.
-     */
-    std::size_t spill(std::size_t amount);
-
-    /**
-     * @brief Attempts to free up memory by spilling data until the requested headroom is
-     * available.
-     *
-     * This method checks the currently available memory and, if insufficient, triggers
-     * spilling mechanisms to free up space. Spilling is performed in order of the
-     * function priorities until the required headroom is reached or no more spilling is
-     * possible.
-     *
-     * @param headroom The target amount of headroom (in bytes). Allowed to be negative.
-     * @return The actual amount of memory spilled (in bytes), which may be less than
-     * requested if there is insufficient spillable data, but may also be more
-     * or equal to requested depending on the sizes of spillable data buffers.
-     */
-    std::size_t spill_to_make_headroom(std::int64_t headroom = 0);
-
-  private:
-    mutable std::mutex mutex_;
-    BufferResource* br_;
-    std::size_t spill_function_id_counter_{0};
-    std::map<SpillFunctionID, SpillFunction> spill_functions_;
-    std::multimap<int, SpillFunctionID, std::greater<>> spill_function_priorities_;
-    std::optional<detail::PausableThreadLoop> periodic_spill_thread_;
 };
 
 /**
--- cpp/src/buffer/resource.cpp
+++ cpp/src/buffer/resource.cpp
@@ -4,8 +4,6 @@
  */
 
 #include <limits>
-#include <optional>
-#include <utility>
 
 #include <rapidsmp/buffer/resource.hpp>
 
@@ -16,79 +14,6 @@ MemoryReservation::~MemoryReservation() noexcept {
     if (size_ > 0) {
         br_->release(*this, mem_type_, size_);
     }
-}
-
-SpillManager::SpillManager(
-    BufferResource* br, std::optional<std::chrono::microseconds> periodic_spill_check
-)
-    : br_{br} {
-    if (periodic_spill_check.has_value()) {
-        periodic_spill_thread_.emplace(
-            [this]() { spill_to_make_headroom(0); }, *periodic_spill_check
-        );
-    }
-}
-
-SpillManager::~SpillManager() {
-    if (periodic_spill_thread_.has_value()) {
-        periodic_spill_thread_->stop();
-    }
-}
-
-SpillManager::SpillFunctionID SpillManager::add_spill_function(
-    SpillFunction spill_function, int priority
-) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    auto const id = spill_function_id_counter_++;
-    RAPIDSMP_EXPECTS(
-        spill_functions_.insert({id, std::move(spill_function)}).second,
-        "corrupted id counter"
-    );
-    spill_function_priorities_.insert({priority, id});
-
-    // Make sure the spill thread is running.
-    if (periodic_spill_thread_.has_value()) {
-        periodic_spill_thread_->resume();
-    }
-    return id;
-}
-
-void SpillManager::remove_spill_function(SpillFunctionID fid) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    auto& prio = spill_function_priorities_;
-    for (auto it = prio.begin(); it != prio.end(); ++it) {
-        if (it->second == fid) {
-            prio.erase(it);  // Erase the first occurrence
-            break;  // Exit after erasing to ensure only the first one is removed
-        }
-    }
-    spill_functions_.erase(fid);
-
-    // Pause the spill thread if no spill functions are left.
-    if (periodic_spill_thread_.has_value() && spill_functions_.empty()) {
-        periodic_spill_thread_->pause();
-    }
-}
-
-std::size_t SpillManager::spill(std::size_t amount) {
-    std::size_t spilled{0};
-    std::lock_guard<std::mutex> lock(mutex_);
-    for (auto const [_, fid] : spill_function_priorities_) {
-        if (spilled >= amount) {
-            break;
-        }
-        spilled += spill_functions_.at(fid)(amount - spilled);
-    }
-    return spilled;
-}
-
-std::size_t SpillManager::spill_to_make_headroom(std::int64_t headroom) {
-    // TODO: check other memory types.
-    std::int64_t available = br_->memory_available(MemoryType::DEVICE)();
-    if (headroom <= available) {
-        return 0;
-    }
-    return spill(headroom - available);
 }
 
 BufferResource::BufferResource(
