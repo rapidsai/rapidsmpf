e0e6449b5f3edb750a9a8af6ddcdbc7af935dbf5
niranda perera <niranda.perera@gmail.com>
2025-04-09 11:38:43 -0700

    more tests

--- cpp/tests/test_chunk_batch.cpp
+++ cpp/tests/test_chunk_batch.cpp
@@ -51,6 +51,11 @@ TEST(ChunkBatch, Empty) {
 
     auto batch2 = ChunkBatch::create(std::move(metadata), {});
     test_empty_batch(batch2);
+
+    // create chunk batch with in invalid metadata buffer -> should throw
+    EXPECT_THROW(
+        ChunkBatch::create(std::make_unique<std::vector<uint8_t>>(), {}), std::logic_error
+    );
 }
 
 /**
@@ -65,30 +70,38 @@ TEST(ChunkBatch, Empty) {
 // Parametarized test for MemoryType
 class ChunkBatchTest : public ::testing::TestWithParam<MemoryType> {
   public:
-    // dummy metadata buffer
-    std::vector<uint8_t> const dummy_data{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // dummy data for buffers
+    static constexpr std::initializer_list<uint8_t> dummy_data{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    static constexpr std::ptrdiff_t len = dummy_data.size();
+
+    // dummy batch details
+    static constexpr uint32_t id = 1;
+    static constexpr Rank rank = 2;
+
     // dummy data buffer
-    std::unique_ptr<Buffer> data;
+    std::unique_ptr<Buffer> data_buf;
 
     rmm::cuda_stream_view stream = rmm::cuda_stream_default;
-    std::ptrdiff_t const len = dummy_data.size();
-
     BufferResource br{cudf::get_current_device_resource_ref(), {{MemoryType::DEVICE, [] {
                                                                      return 1000_KiB;
                                                                  }}}};
 
     ChunkBatchTest() {
-        data = allocate_buffer(GetParam(), dummy_data.size(), br, stream);
-        if (GetParam() == MemoryType::HOST) {
-            std::memcpy(data->data(), dummy_data.data(), dummy_data.size());
-        } else {
+        data_buf = allocate_buffer(GetParam(), dummy_data.size(), br, stream);
+        // copy dummy data to the buffer
+        switch (GetParam()) {
+        case MemoryType::DEVICE:
             RAPIDSMP_CUDA_TRY_ALLOC(cudaMemcpyAsync(
-                data->data(),
-                dummy_data.data(),
+                data_buf->data(),
+                std::data(dummy_data),
                 dummy_data.size(),
                 cudaMemcpyHostToDevice,
                 stream
             ));
+            break;
+        case MemoryType::HOST:
+            std::memcpy(data_buf->data(), std::data(dummy_data), dummy_data.size());
+            break;
         }
     }
 
@@ -97,20 +110,75 @@ class ChunkBatchTest : public ::testing::TestWithParam<MemoryType> {
     }
 
     auto copy_data() const {
-        return data->copy_slice(0, len, stream);
+        return data_buf->copy_slice(0, len, stream);
     }
 
-    auto make_chunks() const {
+    auto make_mixed_chunks() const {
         std::vector<Chunk> chunks{};
         chunks.emplace_back(1, 1, 0, len, copy_metadata(), copy_data());
         chunks.emplace_back(2, 2, 0, len, copy_metadata(), copy_data());
         chunks.emplace_back(3, 3, 100);
         chunks.emplace_back(4, 4, 0, len, copy_metadata(), copy_data());
-        chunks.emplace_back(5, 5, 100);
+        chunks.emplace_back(5, 5, 101);
         chunks.emplace_back(6, 6, 0, len, copy_metadata(), copy_data());
 
         return chunks;
     }
+
+    auto make_no_data_chunks() const {
+        std::vector<Chunk> chunks{};
+        chunks.emplace_back(3, 3, 100);
+        chunks.emplace_back(5, 5, 101);
+        return chunks;
+    }
+
+    void test_batch(std::vector<Chunk> const& exp_chunks, ChunkBatch const& batch) const {
+        EXPECT_EQ(id, batch.id());
+        EXPECT_EQ(rank, batch.destination());
+        EXPECT_EQ(exp_chunks.size(), batch.size());
+
+        std::vector<Chunk> const chunks = batch.get_chunks(stream);
+        EXPECT_EQ(exp_chunks.size(), chunks.size());
+
+        for (size_t i = 0; i < exp_chunks.size(); i++) {
+            SCOPED_TRACE("chunk " + std::to_string(i));
+            EXPECT_EQ(exp_chunks[i].pid, chunks[i].pid);
+            EXPECT_EQ(exp_chunks[i].cid, chunks[i].cid);
+            EXPECT_EQ(exp_chunks[i].expected_num_chunks, chunks[i].expected_num_chunks);
+            EXPECT_EQ(exp_chunks[i].gpu_data_size, chunks[i].gpu_data_size);
+
+            if (exp_chunks[i].metadata) {
+                SCOPED_TRACE("chunk metadata" + std::to_string(i));
+                EXPECT_EQ(*exp_chunks[i].metadata, *chunks[i].metadata);
+            }
+
+            if (exp_chunks[i].gpu_data) {
+                switch (GetParam()) {
+                case MemoryType::DEVICE:
+                    {
+                        SCOPED_TRACE("chunk data device" + std::to_string(i));
+                        auto host_copy = chunks[i].gpu_data->copy_slice(
+                            MemoryType::HOST, 0, len, stream
+                        );
+                        cudaStreamSynchronize(stream);
+                        EXPECT_EQ(
+                            0, std::memcmp(std::data(dummy_data), host_copy->data(), len)
+                        );
+                        break;
+                    }
+                case MemoryType::HOST:
+                    {
+                        SCOPED_TRACE("chunk data host" + std::to_string(i));
+                        EXPECT_EQ(
+                            *(const_cast<Buffer const&>(*exp_chunks[i].gpu_data).host()),
+                            *(const_cast<Buffer const&>(*chunks[i].gpu_data).host())
+                        );
+                        break;
+                    }
+                }
+            }
+        }
+    };
 };
 
 INSTANTIATE_TEST_SUITE_P(
@@ -122,68 +190,24 @@ INSTANTIATE_TEST_SUITE_P(
     }
 );
 
-TEST_P(ChunkBatchTest, NonEmptyDeviceData) {
-    uint32_t id = 1;
-    Rank rank = 2;
+TEST_P(ChunkBatchTest, MixedDataChunks) {
+    std::vector<Chunk> exp_chunks = make_mixed_chunks();
 
-    std::vector<Chunk> exp_chunks = make_chunks();
-
-    auto test_batch = [&](auto const& batch) {
-        EXPECT_EQ(id, batch.id());
-        EXPECT_EQ(rank, batch.destination());
-        EXPECT_EQ(exp_chunks.size(), batch.size());
-
-        std::vector<Chunk> const chunks = batch.get_chunks(stream);
-        EXPECT_EQ(exp_chunks.size(), chunks.size());
-
-        for (size_t i = 0; i < exp_chunks.size(); i++) {
-            SCOPED_TRACE(i);
-            EXPECT_EQ(exp_chunks[i].pid, chunks[i].pid);
-            EXPECT_EQ(exp_chunks[i].cid, chunks[i].cid);
-            EXPECT_EQ(exp_chunks[i].expected_num_chunks, chunks[i].expected_num_chunks);
-            EXPECT_EQ(exp_chunks[i].gpu_data_size, chunks[i].gpu_data_size);
-
-            if (exp_chunks[i].metadata) {
-                EXPECT_EQ(*exp_chunks[i].metadata, *chunks[i].metadata);
-            }
-
-            if (exp_chunks[i].gpu_data) {
-                switch (GetParam()) {
-                case MemoryType::DEVICE:
-                    {
-                        auto host_copy = chunks[i].gpu_data->copy_slice(
-                            MemoryType::HOST, 0, len, stream
-                        );
-                        cudaStreamSynchronize(stream);
-                        EXPECT_EQ(
-                            0, std::memcmp(dummy_data.data(), host_copy->data(), len)
-                        );
-                        break;
-                    }
-                case MemoryType::HOST:
-                    {
-                        EXPECT_EQ(
-                            *(const_cast<Buffer const&>(*exp_chunks[i].gpu_data).host()),
-                            *(const_cast<Buffer const&>(*chunks[i].gpu_data).host())
-                        );
-                        EXPECT_EQ(
-                            0,
-                            std::memcmp(
-                                dummy_data.data(), chunks[i].gpu_data->data(), len
-                            )
-                        );
-                        break;
-                    }
-                }
-            }
-        }
-    };
-
-    auto batch1 = ChunkBatch::create(id, rank, make_chunks(), &br, stream);
-    test_batch(batch1);
+    auto batch1 = ChunkBatch::create(id, rank, make_mixed_chunks(), &br, stream);
+    test_batch(exp_chunks, batch1);
 
     auto batch2 = ChunkBatch::create(batch1.release_metadata(), batch1.release_payload());
-    test_batch(batch2);
+    test_batch(exp_chunks, batch2);
+}
+
+TEST_P(ChunkBatchTest, NoDataChunks) {
+    std::vector<Chunk> exp_chunks = make_no_data_chunks();
+
+    auto batch1 = ChunkBatch::create(id, rank, make_no_data_chunks(), &br, stream);
+    test_batch(exp_chunks, batch1);
+
+    auto batch2 = ChunkBatch::create(batch1.release_metadata(), batch1.release_payload());
+    test_batch(exp_chunks, batch2);
 }
 
 
