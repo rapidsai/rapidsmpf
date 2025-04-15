91bd4b5ecfbc21bacd1561cd2e75588974d02bcd
Peter Andreas Entschev <peter@entschev.com>
2025-04-15 16:07:49 +0200

    Fix typo
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 
 project = 'rapidsmpf'
 copyright = '2025, NVIDA Corporation'
-author = 'NVIDA Corporation'
+author = 'NVIDIA Corporation'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
