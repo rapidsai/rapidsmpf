f5dbdc5a859e95cbc81d16ed63a08afb70b6b467
Sebastian Berg <sebastianb@nvidia.com>
2025-05-08 08:44:59 -0700

    Install rapidsmpf targetted in conda build
    I don't think this is quite ideal, but the test builds somehow
    end up being installed (i.e. the COMPONENT works differently there)?
    
    Signed-off-by: Sebastian Berg <sebastianb@nvidia.com>

--- conda/recipes/librapidsmpf/install_librapidsmpf.sh
+++ conda/recipes/librapidsmpf/install_librapidsmpf.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 
-cmake --install cpp/build
+cmake --install cpp/build --component rapidsmpf
