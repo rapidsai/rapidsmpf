cff210e1229b0a9448da31848508cd2ccb44cf5b
Mads R. B. Kristensen <madsbk@gmail.com>
2025-05-08 17:37:47 +0200

    doc

--- python/rapidsmpf/rapidsmpf/communicator/__init__.py
+++ python/rapidsmpf/rapidsmpf/communicator/__init__.py
@@ -1,21 +1,18 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
-"""
-Submodule for communication abstraction (e.g. UCXX and MPI).
-
-Attributes
-----------
-MPI_SUPPORT
-    Whether the MPI communicator (``rapidsmpf.communicator.mpi``) is available. This is
-    False when RapidsMPF wasn't built with MPI support. Typically, MPI is supported in
-    the Conda package but not in the PIP package.
-"""
+"""Submodule for communication abstraction (e.g. UCXX and MPI)."""
 
 from __future__ import annotations
 
 import importlib.util
 
 MPI_SUPPORT: bool = importlib.util.find_spec("rapidsmpf.communicator.mpi") is not None
+"""Whether the MPI communicator (``rapidsmpf.communicator.mpi``) is available.
+
+This is false when RapidsMPF wasn't built with MPI support. Typically, MPI is
+supported in the Conda package but not in the PIP package.
+"""
+
 
 __all__ = [
     "MPI_SUPPORT",
