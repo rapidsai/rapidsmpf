66b72a9919cd054059480fca5eade656b6bfd887
Mads R. B. Kristensen <madsbk@gmail.com>
2025-05-09 14:08:19 +0200

    doc

--- python/rapidsmpf/rapidsmpf/communicator/__init__.py
+++ python/rapidsmpf/rapidsmpf/communicator/__init__.py
@@ -9,9 +9,9 @@ from rapidsmpf.communicator.communicator import _available_communicators
 COMMUNICATORS = _available_communicators()
 """Tuple of available communicators.
 
-RapidsMPF if built with a set of communicators, which can be found as submodules
-under ``rapidsmpf.communicator.*``. Typically, the Conda package is built with
-both UCXX and MPI support whereas the PIP package only supports UCXX.
+RapidsMPF includes a collection of communicator backends, available as submodules
+under ``rapidsmpf.communicator.*``. Typically, the Conda distribution includes
+both UCXX and MPI support, while the PIP installation generally supports only UCXX.
 """
 
 
