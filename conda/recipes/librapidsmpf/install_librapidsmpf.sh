4383f3d31569c635be1c7d01f5ddf516adda73e5
Sebastian Berg <sebastianb@nvidia.com>
2025-05-08 23:34:05 -0700

    Use EXCLUDE_FROM_ALL and move benchmarks to main package
    Signed-off-by: Sebastian Berg <sebastianb@nvidia.com>

--- conda/recipes/librapidsmpf/install_librapidsmpf.sh
+++ conda/recipes/librapidsmpf/install_librapidsmpf.sh
@@ -2,4 +2,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 
-cmake --install cpp/build --component rapidsmpf
+cmake --install cpp/build
+cmake --install cpp/build --component=benchmarking
