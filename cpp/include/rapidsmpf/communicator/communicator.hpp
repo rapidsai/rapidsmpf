0f762f6934150118e1f730c2d41803bf99c94a03
Mads R. B. Kristensen <madsbk@gmail.com>
2025-05-12 16:01:37 +0200

    move level_from_env to .cpp

--- cpp/include/rapidsmpf/communicator/communicator.hpp
+++ cpp/include/rapidsmpf/communicator/communicator.hpp
@@ -201,36 +201,6 @@ class Communicator {
         }
 
         /**
-         * @brief Get the verbosity level from the environment variable `RAPIDSMPF_LOG`.
-         *
-         * This function reads the `RAPIDSMPF_LOG` environment variable, trims whitespace,
-         * converts the value to uppercase, and attempts to match it against known logging
-         * level names. If the environment variable is not set, the default value `"WARN"`
-         * is used.
-         *
-         * @return The corresponding logging level of type `LOG_LEVEL`.
-         *
-         * @throws std::invalid_argument If the environment variable contains an unknown
-         * value.
-         */
-        static LOG_LEVEL level_from_env() {
-            auto env = to_upper(trim(getenv_or<std::string>("RAPIDSMPF_LOG", "WARN")));
-            for (std::uint32_t i = 0; i < LOG_LEVEL_NAMES.size(); ++i) {
-                auto level = static_cast<LOG_LEVEL>(i);
-                if (env == level_name(level)) {
-                    return level;
-                }
-            }
-            std::stringstream ss;
-            ss << "RAPIDSMPF_LOG - unknown value: \"" << env << "\", valid choices: { ";
-            for (auto const& name : LOG_LEVEL_NAMES) {
-                ss << name << " ";
-            }
-            ss << "}";
-            throw std::invalid_argument(ss.str());
-        }
-
-        /**
          * @brief Construct a new logger.
          *
          * To control the verbosity level, set the environment variable `RAPIDSMPF_LOG`:
--- cpp/src/communicator/communicator.cpp
+++ cpp/src/communicator/communicator.cpp
@@ -7,7 +7,42 @@
 
 namespace rapidsmpf {
 
+namespace detail {
+
+
+/**
+ * @brief Get the verbosity level from the environment variable `RAPIDSMPF_LOG`.
+ *
+ * This function reads the `RAPIDSMPF_LOG` environment variable, trims whitespace,
+ * converts the value to uppercase, and attempts to match it against known logging
+ * level names. If the environment variable is not set, the default value `"WARN"`
+ * is used.
+ *
+ * @return The corresponding logging level of type `LOG_LEVEL`.
+ *
+ * @throws std::invalid_argument If the environment variable contains an unknown
+ * value.
+ */
+Communicator::Logger::LOG_LEVEL level_from_env() {
+    auto env = to_upper(trim(getenv_or<std::string>("RAPIDSMPF_LOG", "WARN")));
+    for (std::uint32_t i = 0; i < Communicator::Logger::LOG_LEVEL_NAMES.size(); ++i) {
+        auto level = static_cast<Communicator::Logger::LOG_LEVEL>(i);
+        if (env == Communicator::Logger::level_name(level)) {
+            return level;
+        }
+    }
+    std::stringstream ss;
+    ss << "RAPIDSMPF_LOG - unknown value: \"" << env << "\", valid choices: { ";
+    for (auto const& name : Communicator::Logger::LOG_LEVEL_NAMES) {
+        ss << name << " ";
+    }
+    ss << "}";
+    throw std::invalid_argument(ss.str());
+}
+}  // namespace detail
+
 Communicator::Logger::Logger(Communicator* comm)
-    : comm_{comm}, level_{level_from_env()} {};
+    : comm_{comm}, level_{detail::level_from_env()} {};
+
 
 }  // namespace rapidsmpf
