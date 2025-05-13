0cd70a27bff2b12f1677eccd6fa38dbc90e58886
Gil Forsyth <gforsyth@nvidia.com>
2025-05-13 11:31:08 -0400

    feat: add shared object loader

--- python/rapidsmpf/rapidsmpf/__init__.py
+++ python/rapidsmpf/rapidsmpf/__init__.py
@@ -1,3 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 """Collection of multi-gpu, distributed memory algorithms."""
+
+from __future__ import annotations
+
+from rapidsmpf.load import load_library
+
+load_library()
