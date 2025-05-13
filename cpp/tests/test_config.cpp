ad4bc4e874cfeba783fcd8103a5621494d41032a
Mads R. B. Kristensen <madsbk@gmail.com>
2025-05-13 14:51:46 +0200

    MockOptionNotUsed

--- cpp/tests/test_config.cpp
+++ cpp/tests/test_config.cpp
@@ -17,6 +17,13 @@ class MockOption : public Option {
     std::string value;
 };
 
+class MockOptionNotUsed : public Option {
+  public:
+    explicit MockOptionNotUsed(std::string value = "") : value(std::move(value)) {}
+
+    std::string value;
+};
+
 TEST(Options, RetrieveExistingOption) {
     std::unordered_map<std::string, std::unique_ptr<Option>> options;
     options["key"] = std::make_unique<MockOption>("value");
@@ -53,7 +60,7 @@ TEST(Options, InvalidTypeAccess) {
 
     Options opts(std::move(options));
 
-    EXPECT_THROW(opts.get<int>("key"), std::invalid_argument);
+    EXPECT_THROW(opts.get<MockOptionNotUsed>("key"), std::invalid_argument);
 }
 
 TEST(Options, MissingKeyThrowsNoException) {
