78a478f22e0f1fa9757a436cdeef38376e11d0ca
Gil Forsyth <gforsyth@nvidia.com>
2025-05-19 11:49:22 -0400

    feat: port conda recipes to `rattler-build`
    first cut of `rapidsmpf`
    
    ci: use `rattler-build`
    
    remove old config files

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -4,8 +4,6 @@
 
 set -euo pipefail
 
-rapids-configure-conda-channels
-
 source rapids-configure-sccache
 
 source rapids-date-string
@@ -18,12 +16,27 @@ rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
 
+RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION)
+export RAPIDS_PACKAGE_VERSION
+
+# Define this before sourcing `rapids-rattler-channel-string` to prepend
+RAPIDS_PREPENDED_CONDA_CHANNELS=(CPP_CHANNEL)
+
+# populates `RATTLER_CHANNELS` array and `RATTLER_ARGS` array
+source rapids-rattler-channel-string
+
 sccache --zero-stats
 
-RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry build \
-  --channel "${CPP_CHANNEL}" \
-  conda/recipes/rapidsmpf
+rapids-logger "Building rapidsmpf"
+
+rattler-build build --recipe conda/recipes/rapidsmpf \
+                    "${RATTLER_ARGS[@]}" \
+                    "${RATTLER_CHANNELS[@]}"
 
 sccache --show-adv-stats
+
+# remove build_cache directory to avoid uploading the entire source tree
+# tracked in https://github.com/prefix-dev/rattler-build/issues/1424
+rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
 
 rapids-upload-conda-to-s3 python
