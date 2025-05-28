01f76679af41e98221859e66459086c1d39cd7a3
Gil Forsyth <gforsyth@nvidia.com>
2025-05-28 12:37:42 -0400

    refactor: don't use rapids-generate-pip-constraints (it doesn't handle `cuda_suffixed`)

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -9,13 +9,12 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="librapidsmpf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 PYTHON_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rapidsmpf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
 
-# generate constraints (possibly pinning to oldest support versions of dependencies)
-rapids-generate-pip-constraints test_python ./constraints.txt
+# generate test dependencies
+rapids-dependency-file-generator --output requirements --file-key "test_python" --matrix "cuda=12.*;arch=x86_64;cuda_suffixed=true;py=313" | tee -a "$PIP_CONSTRAINT"
 
 # echo to expand wildcard before adding '[extra]' requires for pip
 rapids-pip-retry install \
     -v \
-    --constraint ./constraints.txt \
     --constraint "${PIP_CONSTRAINT}" \
     "${CPP_WHEELHOUSE}"/*.whl \
     "$(echo "${PYTHON_WHEELHOUSE}"/rapidsmpf_"${RAPIDS_PY_CUDA_SUFFIX}"*.whl)[test]"
