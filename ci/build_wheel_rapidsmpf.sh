54b625cde4ff5a4493a733cbcc9146aa4cdd828f
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-05-29 08:02:25 -0400

    feat: add wheel build of `rapidsmpf` (#269)
    xref #219
    
    Part of rapidsai/build-planning#170
    
    Adds wheels for `rapidsmpf`.
    
    Currently the `librapidsmpf` and `rapidsmpf` wheels are combined into a single file.
    
    My CMake isn't great, and for now I'm hacking in some paths with an environment variable, just so I can start testing this in CI, but I'll work on something more idiomatic.
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Tom Augspurger (https://github.com/TomAugspurger)
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rapidsmpf/pull/269

---
+++ ci/build_wheel_rapidsmpf.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+package_name="rapidsmpf"
+package_dir="python/rapidsmpf"
+source rapids-init-pip
+
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
+
+
+LIBRAPIDSMPF_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="librapidsmpf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
+echo "librapidsmpf-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBRAPIDSMPF_WHEELHOUSE}"/librapidsmpf_*.whl)" >> "${PIP_CONSTRAINT}"
+
+rapids-logger "Generating build requirements"
+
+rapids-dependency-file-generator \
+  --output requirements \
+  --file-key "py_build_${package_name}" \
+  --file-key "py_rapids_build_${package_name}" \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION};cuda_suffixed=true" \
+| tee /tmp/requirements-build.txt
+
+rapids-logger "Installing build requirements"
+rapids-pip-retry install \
+    -v \
+    --prefer-binary \
+    -r /tmp/requirements-build.txt
+
+# build with '--no-build-isolation', for better sccache hit rate
+# 0 really means "add --no-build-isolation" (ref: https://github.com/pypa/pip/issues/5735)
+export PIP_NO_BUILD_ISOLATION=0
+
+export SKBUILD_CMAKE_ARGS=""
+
+SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
+export SITE_PACKAGES
+
+./ci/build_wheel.sh "${package_name}" "${package_dir}"
+
+python -m auditwheel repair \
+    --exclude libcudf.so \
+    --exclude librapids_logger.so \
+    --exclude librmm.so \
+    --exclude libucp.so.0 \
+    --exclude libucxx.so \
+    --exclude libucxx_python.so \
+    --exclude librapidsmpf.so \
+    -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" \
+    ${package_dir}/dist/*
+
+./ci/validate_wheel.sh "${package_dir}" "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
