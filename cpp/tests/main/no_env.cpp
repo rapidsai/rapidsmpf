bd5ee9680cd60e1fdc16cd7abce620e17f97d52d
niranda perera <niranda.perera@gmail.com>
2025-06-02 13:24:07 -0700

    adding a standalone test
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

---
+++ cpp/tests/main/no_env.cpp
@@ -0,0 +1,11 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
