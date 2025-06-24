a66a6866a098f858cf0abf6939cb27d3a3170f39
Mads R. B. Kristensen <madsbk@gmail.com>
2025-06-24 14:41:12 +0200

    cleanup

--- cpp/src/statistics.cpp
+++ cpp/src/statistics.cpp
@@ -79,9 +79,6 @@ Statistics::MemoryRecorder::MemoryRecorder(
 }
 
 Statistics::MemoryRecorder::~MemoryRecorder() {
-    if (stats_ == nullptr) {
-        return;
-    }
     auto const scope = mr_->end_scoped_memory_record();
 
     std::lock_guard<std::mutex> lock(stats_->mutex_);
