d7cdcc2cedcca013334ad4eda669d5afc0223ec2
Mads R. B. Kristensen <madsbk@gmail.com>
2025-06-24 17:34:45 +0200

    track all threads

--- cpp/src/locking.cpp
+++ cpp/src/locking.cpp
@@ -10,8 +10,23 @@
 #include <rapidsmpf/locking.hpp>
 
 namespace rapidsmpf::detail {
+namespace {
 
-static thread_local std::map<uintptr_t, std::tuple<char const*, int>> held_locks{};
+using held_locks_t = std::map<uintptr_t, std::tuple<char const*, int>>;
+
+std::mutex held_locks_by_thread_mutex;
+std::map<std::thread::id, std::unique_ptr<held_locks_t>> held_locks_by_thread{};
+
+held_locks_t& get_locks_held_by_thread() {
+    std::lock_guard<std::mutex> const lock(held_locks_by_thread_mutex);
+    auto& held_ptr = held_locks_by_thread[std::this_thread::get_id()];
+    if (!held_ptr) {
+        held_ptr = std::make_unique<held_locks_t>();
+    }
+    return *held_ptr;
+}
+
+};  // namespace
 
 timeout_lock_guard::timeout_lock_guard(
     std::timed_mutex& mutex,
@@ -26,19 +41,23 @@ timeout_lock_guard::timeout_lock_guard(
            << " blocking (" << std::dec << timeout.count() << " s) at " << filename << ":"
            << line_number;
         ss << "\n  thread is trying to acquire mutex 0x" << std::hex << mid_ << std::dec;
-        ss << "\n  thread holds the following mutexes already:\n";
-        for (auto& [mutex_id, info] : held_locks) {
-            auto& [filename, line_number] = info;
-            ss << "    " << std::hex << "mutex 0x" << mutex_id << " at " << filename
-               << ":" << std::dec << line_number << "\n";
+        ss << ", the following is all known acquired mutexes:\n";
+        std::lock_guard<std::mutex> const lock(held_locks_by_thread_mutex);
+        for (auto& [thread_id, held_locks] : held_locks_by_thread) {
+            for (auto& [mutex_id, info] : *held_locks) {
+                auto& [filename, line_number] = info;
+                ss << "    " << std::hex << "thread 0x" << thread_id
+                   << " acquired mutex 0x" << mutex_id << " at " << filename << ":"
+                   << std::dec << line_number << "\n";
+            }
         }
         std::cerr << ss.str() << std::endl;
     }
-    held_locks[mid_] = {filename, line_number};
+    get_locks_held_by_thread()[mid_] = {filename, line_number};
 }
 
 timeout_lock_guard::~timeout_lock_guard() {
-    held_locks.erase(mid_);
+    get_locks_held_by_thread().erase(mid_);
 }
 
 }  // namespace rapidsmpf::detail
