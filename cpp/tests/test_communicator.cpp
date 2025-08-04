d681e7da4db0b93670ed7d93d294d9e6fa8421e6
niranda perera <niranda.perera@gmail.com>
2025-08-04 12:10:47 -0700

    add test
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- cpp/tests/test_communicator.cpp
+++ cpp/tests/test_communicator.cpp
@@ -93,3 +93,148 @@ TEST_P(BasicCommunicatorTest, SendToSelf) {
     stream.synchronize();
     EXPECT_EQ(send_data_h, *recv_data_h);
 }
+
+class CommunicatorTest
+    : public BaseCommunicatorTest,
+      public ::testing::WithParamInterface<std::tuple<rapidsmpf::MemoryType, size_t>> {
+  protected:
+    void SetUp() override {
+        BaseCommunicatorTest::SetUp();
+        std::tie(mem_type, n_ops) = GetParam();
+    }
+
+    rapidsmpf::MemoryType memory_type() override {
+        return std::get<0>(GetParam());
+    }
+
+    auto make_buffer(size_t size) {
+        if (mem_type == rapidsmpf::MemoryType::HOST) {
+            return br->move(std::make_unique<std::vector<uint8_t>>(size));
+        } else {
+            return br->move(std::make_unique<rmm::device_buffer>(size, stream), stream);
+        }
+    }
+
+    auto copy_to_buffer(void* src, size_t size, rapidsmpf::Buffer& buf) {
+        if (mem_type == rapidsmpf::MemoryType::HOST) {
+            std::memcpy(buf.data(), src, size);
+        } else {
+            RAPIDSMPF_CUDA_TRY(
+                cudaMemcpyAsync(buf.data(), src, size, cudaMemcpyDefault, stream)
+            );
+            RAPIDSMPF_CUDA_TRY(cudaStreamSynchronize(stream));
+        }
+    }
+
+    auto copy_from_buffer(rapidsmpf::Buffer& buf, void* dst, size_t size) {
+        if (mem_type == rapidsmpf::MemoryType::HOST) {
+            std::memcpy(dst, buf.data(), size);
+        } else {
+            RAPIDSMPF_CUDA_TRY(
+                cudaMemcpyAsync(dst, buf.data(), size, cudaMemcpyDefault, stream)
+            );
+            RAPIDSMPF_CUDA_TRY(cudaStreamSynchronize(stream));
+        }
+    }
+
+    rapidsmpf::MemoryType mem_type;
+    size_t n_ops;
+};
+
+INSTANTIATE_TEST_CASE_P(
+    CommunicatorTest,
+    CommunicatorTest,
+    testing::Combine(
+        testing::Values(rapidsmpf::MemoryType::HOST, rapidsmpf::MemoryType::DEVICE),
+        testing::Values(1, 3)
+    ),
+    [](const testing::TestParamInfo<std::tuple<rapidsmpf::MemoryType, size_t>>& info) {
+        return "memory_type_" + std::to_string(static_cast<int>(std::get<0>(info.param)))
+               + "_n_ops_" + std::to_string(std::get<1>(info.param));
+    }
+);
+
+// Test every rank sends a buffer to all other ranks (including itself)
+TEST_P(CommunicatorTest, MultiDestinationSend) {
+    if (GlobalEnvironment->type() == TestEnvironmentType::SINGLE) {
+        GTEST_SKIP() << "SINGLE communicator does not support multi-destination send";
+    }
+
+    auto log_vec = [&](std::string const& prefix, std::vector<int> const& vec) {
+        std::stringstream ss;
+        ss << prefix << " ";
+        for (auto&& v : vec) {
+            ss << v << " ";
+        }
+        comm->logger().debug(ss.str());
+    };
+
+    constexpr size_t n_elems = 5;  // number of int elements to send
+    auto const all_ranks = iota_vector<rapidsmpf::Rank>(comm->nranks());
+
+    // send data is arranged as follows:
+    // | op 0                     | op 1                             | ... |
+    // | rank 0  |  rank 1  | ... | rank 0            | rank 1 | ... |
+    // | 0...n-1 | n...2n-1 | ... | n_ranks * n..     |...
+
+    // for each operation, each rank sends to every other rank, using the op iteration as
+    // the op and rank as the stage of the tag
+    std::vector<std::unique_ptr<rapidsmpf::Communicator::BatchFuture>> send_futures;
+    std::vector<std::unique_ptr<rapidsmpf::Communicator::Future>> recv_futures;
+    for (rapidsmpf::OpID op = 0; op < n_ops; ++op) {
+        // every ranks receives from the other ranks. Post all receives first.
+        for (rapidsmpf::Rank sender : all_ranks) {
+            auto recv_buf = make_buffer(n_elems * sizeof(int));
+            recv_futures.emplace_back(comm->recv(
+                sender,
+                rapidsmpf::Tag{op, static_cast<rapidsmpf::StageID>(sender)},
+                std::move(recv_buf)
+            ));
+        }
+
+        rapidsmpf::Rank this_rank = comm->rank();
+        auto send_data =
+            iota_vector<int>(n_elems, n_elems * (op * comm->nranks() + this_rank));
+
+        std::unordered_set<rapidsmpf::Rank> receivers(all_ranks.begin(), all_ranks.end());
+        // Post batch send
+        auto send_buf = make_buffer(n_elems * sizeof(int));
+        copy_to_buffer(send_data.data(), n_elems * sizeof(int), *send_buf);
+        send_futures.emplace_back(comm->send(
+            std::move(send_buf),
+            receivers,
+            rapidsmpf::Tag{op, static_cast<rapidsmpf::StageID>(this_rank)}
+        ));
+    }
+
+    // wait for all sends to complete
+    while (!std::ranges::all_of(send_futures, [&](auto& future) {
+        return comm->test_batch(*future);
+    }))
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+
+    // wait for all receives to complete
+    std::vector<int> recv_data(n_elems * comm->nranks() * n_ops);
+    size_t offset = 0;
+    while (!recv_futures.empty()) {
+        auto finished = comm->test_some(recv_futures);
+        for (auto&& f : finished) {
+            auto recv_buf = comm->get_gpu_data(std::move(f));
+            copy_from_buffer(*recv_buf, recv_data.data() + offset, n_elems * sizeof(int));
+            log_vec(" offset: " + std::to_string(offset) + ":", recv_data);
+            offset += n_elems;
+        }
+    }
+    EXPECT_EQ(offset, n_elems * comm->nranks() * n_ops);
+
+    // sort recv data
+    std::ranges::sort(recv_data);
+
+    // check if the recv data is sorted
+    for (int i = 0; i < static_cast<int>(recv_data.size()); ++i) {
+        EXPECT_EQ(i, recv_data[i]);
+    }
+    log_vec("recv_data:", recv_data);
+}
