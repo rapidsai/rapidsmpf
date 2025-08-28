5651dc87bc032c90603f81553a7837e13fb56090
James Lamb <jlamb@nvidia.com>
2025-08-28 12:18:41 -0500

    fix test dependencies

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -5,12 +5,10 @@ set -eou pipefail
 
 source rapids-init-pip
 
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="librapidsmpf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 PYTHON_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rapidsmpf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
-
-# generate test dependencies
-rapids-dependency-file-generator --output requirements --file-key "test_python" --matrix "cuda=12.*;arch=x86_64;cuda_suffixed=true;py=313" | tee -a "$PIP_CONSTRAINT"
 
 # echo to expand wildcard before adding '[extra]' requires for pip
 rapids-pip-retry install \
