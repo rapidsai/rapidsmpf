b1dac296af001b0856f650ce0c2c68ea23c9cc04
Mads R. B. Kristensen <madsbk@gmail.com>
2025-09-04 15:25:02 +0200

    Update cpp/include/rapidsmpf/shuffler/finish_counter.hpp
    Co-authored-by: Peter Andreas Entschev <peter@entschev.com>

--- cpp/include/rapidsmpf/shuffler/finish_counter.hpp
+++ cpp/include/rapidsmpf/shuffler/finish_counter.hpp
@@ -96,8 +96,8 @@ class FinishCounter {
      *
      * @return The partition ID of a finished partition.
      *
-     * @throw std::out_of_range If all partitions have already been waited on.
-     * std::runtime_error If timeout was set and no partitions have been finished by the
+     * @throws std::out_of_range If all partitions have already been waited on.
+     * @throws std::runtime_error If timeout was set and no partitions have been finished by the
      * expiration.
      */
     PartID wait_any(std::optional<std::chrono::milliseconds> timeout = {});
