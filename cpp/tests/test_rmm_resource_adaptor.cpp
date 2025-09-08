4dc6bbbaff74e38dd4a71b5283e166665af7e107
Mads R. B. Kristensen <madsbk@gmail.com>
2025-09-08 22:44:10 +0200

    CI: run with compute sanitizer (#498)
    Run compute-sanitizer in CI. 
    Because of limited CI resources, we only run with the single-communicator. It only takes 4mins to run the tests.
    
    Also fixing a free-bug in a test (found by compute-sanitizer).
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - Niranda Perera (https://github.com/nirandaperera)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
      - Niranda Perera (https://github.com/nirandaperera)
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/rapidsmpf/pull/498

--- cpp/tests/test_rmm_resource_adaptor.cpp
+++ cpp/tests/test_rmm_resource_adaptor.cpp
@@ -37,8 +37,8 @@ struct throw_at_limit_resource final : public rmm::mr::device_memory_resource {
         return ptr;
     }
 
-    void do_deallocate(void* ptr, std::size_t, rmm::cuda_stream_view) override {
-        RAPIDSMPF_CUDA_TRY(cudaFree(ptr));
+    void do_deallocate(void* ptr, std::size_t, rmm::cuda_stream_view stream) override {
+        RAPIDSMPF_CUDA_TRY(cudaFreeAsync(ptr, stream.value()));
         allocs.erase(ptr);
     }
 
