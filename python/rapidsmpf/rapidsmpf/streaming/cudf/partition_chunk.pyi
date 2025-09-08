d9390294cca14c333f461ae151852a62f7756faf
Mads R. B. Kristensen <madsbk@gmail.com>
2025-09-08 11:25:55 +0200

    Python bindings for the streaming shuffler (#490)
    Also adding bindings to `PartitionVectorChunk`
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
      - Peter Andreas Entschev (https://github.com/pentschev)
      - Lawrence Mitchell (https://github.com/wence-)
    
    URL: https://github.com/rapidsai/rapidsmpf/pull/490

--- python/rapidsmpf/rapidsmpf/streaming/cudf/partition_chunk.pyi
+++ python/rapidsmpf/rapidsmpf/streaming/cudf/partition_chunk.pyi
@@ -17,7 +17,20 @@ class PartitionMapChunk:
     @property
     def stream(self) -> Stream: ...
 
+class PartitionVectorChunk:
+    @classmethod
+    def from_message(cls, message: Message[Self]) -> Self: ...
+    def into_message(self, message: Message[Self]) -> None: ...
+    @property
+    def sequence_number(self) -> int: ...
+    @property
+    def stream(self) -> Stream: ...
+
 if TYPE_CHECKING:
     # Check that PartitionMapChunk implements Payload.
     t1: PartitionMapChunk
     t2: Payload = t1
+
+    # Check that PartitionVectorChunk implements Payload.
+    t3: PartitionVectorChunk
+    t4: Payload = t3
