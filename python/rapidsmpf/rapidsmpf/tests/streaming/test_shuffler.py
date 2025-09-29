430aedde767af3ff79289362fe47fbfb79df63f9
Lawrence Mitchell <lmitchell@nvidia.com>
2025-09-29 09:23:23 +0000

    Move streaming shuffler node
    These don't depend on cudf so shouldn't be in the cudf subdirectory.

--- python/rapidsmpf/rapidsmpf/tests/streaming/test_shuffler.py
+++ python/rapidsmpf/rapidsmpf/tests/streaming/test_shuffler.py
@@ -10,11 +10,11 @@ import pytest
 
 import cudf
 
+from rapidsmpf.streaming.coll.shuffler import shuffler
 from rapidsmpf.streaming.core.channel import Channel, Message
 from rapidsmpf.streaming.core.leaf_node import pull_from_channel, push_to_channel
 from rapidsmpf.streaming.core.node import run_streaming_pipeline
 from rapidsmpf.streaming.cudf.partition import partition_and_pack, unpack_and_concat
-from rapidsmpf.streaming.cudf.shuffler import shuffler
 from rapidsmpf.streaming.cudf.table_chunk import TableChunk
 from rapidsmpf.testing import assert_eq
 from rapidsmpf.utils.cudf import cudf_to_pylibcudf_table, pylibcudf_to_cudf_dataframe
