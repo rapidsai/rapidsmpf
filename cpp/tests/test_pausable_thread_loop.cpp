9ab1528ebb09a4d17e0e782b9f6cbd743d66d03b
Niranda Perera <niranda.perera@gmail.com>
2025-10-16 09:07:53 -0700

    Lock-free pausable thread loop (#583)
    This PR is making the pausable thread loop lock-free using atomic operations. 
    
    supersedes #565   
    
    Fixes #578
    
    Authors:
      - Niranda Perera (https://github.com/nirandaperera)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/rapidsmpf/pull/583

--- cpp/tests/test_pausable_thread_loop.cpp
+++ cpp/tests/test_pausable_thread_loop.cpp
@@ -3,9 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <array>
 #include <chrono>
 #include <condition_variable>
+#include <future>
 #include <mutex>
+#include <random>
+#include <ranges>
+#include <vector>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -51,4 +56,76 @@ TEST(PausableThreadLoop, ResumeAndPause) {
     EXPECT_THAT(
         count_after_pause, testing::AnyOf(testing::Eq(counter), testing::Eq(counter - 1))
     );
+}
+
+TEST(PausableThreadLoop, MultiplePauseAndResume) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distr(5, 10);
+
+    PausableThreadLoop loop([&] {
+        // thread sleeps for a [5, 10) milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(distr(gen)));
+    });
+
+    std::array<std::future<void>, 8> futures{
+        std::async(std::launch::async, [&] { loop.pause(); }),
+        std::async(std::launch::async, [&] { loop.resume(); }),
+        std::async(std::launch::async, [&] { loop.pause_nb(); }),
+        std::async(std::launch::async, [&] { loop.resume(); }),
+        std::async(std::launch::async, [&] { loop.pause(); }),
+        std::async(std::launch::async, [&] { loop.resume(); }),
+        std::async(std::launch::async, [&] { loop.pause_nb(); }),
+        std::async(std::launch::async, [&] { loop.resume(); })
+    };
+
+    std::ranges::for_each(futures, [](auto& f) { f.get(); });
+
+    // loop could be running/paused. But all calls should have completed.
+    loop.stop();
+    EXPECT_FALSE(loop.is_running());
+}
+
+TEST(PausableThreadLoop, ChaoticOperations) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distr(5, 10);
+
+    PausableThreadLoop loop([&] {
+        // thread sleeps for a [5, 10) milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(distr(gen)));
+    });
+    EXPECT_TRUE(loop.resume());
+
+    std::vector<std::future<void>> futures;
+
+    std::atomic<int> stop_count{0};
+    for (auto i : std::views::iota(0, 10)) {
+        futures.push_back(std::async(std::launch::async, [&] { loop.pause(); }));
+        futures.push_back(std::async(std::launch::async, [&] {
+            auto ret = loop.resume();
+            if (stop_count > 0) {
+                EXPECT_FALSE(ret);
+            }
+        }));
+        futures.push_back(std::async(std::launch::async, [&] { loop.pause_nb(); }));
+        futures.push_back(std::async(std::launch::async, [&] {
+            auto ret = loop.resume();
+            if (stop_count > 0) {
+                EXPECT_FALSE(ret);
+            }
+        }));
+        // call stop in last 2 iterations. After this stop call, every other operation
+        // should do nothing.
+        if (i >= 8) {
+            futures.push_back(std::async(std::launch::async, [&] {
+                stop_count += loop.stop();
+            }));
+        }
+    }
+
+    std::ranges::for_each(futures, [](auto& f) { f.get(); });
+
+    EXPECT_FALSE(loop.is_running());
+    EXPECT_EQ(1, stop_count);  // loop should be stopped by one of the stop calls
 }
