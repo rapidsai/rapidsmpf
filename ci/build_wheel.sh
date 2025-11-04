c7615e276bed1da38f017cd1aec583dc46f3a47e
ptaylor <paul.e.taylor@me.com>
2025-11-04 20:42:09 +0000

    only call rapids-configure-sccache once

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 package_name=$1
 package_dir=$2
 
-source rapids-configure-sccache
 source rapids-date-string
 source rapids-init-pip
 
--- ci/build_wheel_librapidsmpf.sh
+++ ci/build_wheel_librapidsmpf.sh
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
+source rapids-configure-sccache
 source rapids-init-pip
 
 package_name="librapidsmpf"
