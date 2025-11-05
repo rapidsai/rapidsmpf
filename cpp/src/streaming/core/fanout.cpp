6ea737c157555fcdd3912d65021514abe159cd67
niranda perera <niranda.perera@gmail.com>
2025-11-04 17:03:41 -0800

    WIP
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- cpp/src/streaming/core/fanout.cpp
+++ cpp/src/streaming/core/fanout.cpp
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <algorithm>
+
 #include <rapidsmpf/streaming/core/coro_utils.hpp>
 #include <rapidsmpf/streaming/core/fanout.hpp>
 
@@ -30,6 +32,123 @@ Node send_to_channels(
     }
     coro_results(co_await coro::when_all(std::move(tasks)));
 }
+
+struct UnboundedFanoutState {
+    UnboundedFanoutState(std::vector<std::shared_ptr<Channel>>&& chs_out_)
+        : chs_out{std::move(chs_out_)},
+          ch_next_idx{chs_out.size(), 0},
+          ch_data_avail{chs_out.size(), {}},
+          send_tasks{chs_out.size()} {}
+
+    coro::task<void> receive_done() {
+        auto lock = co_await mtx.scoped_lock();
+        n_msgs = chs_out.size();
+    }
+
+    [[nodiscard]] constexpr bool all_received() const {
+        return n_msgs != std::numeric_limits<size_t>::max();
+    }
+
+    [[nodiscard]] constexpr bool all_sent(size_t i) const {
+        return all_received() && ch_next_idx[i] == n_msgs;
+    }
+
+    // [[nodiscard]] constexpr size_t last_completed_idx() const {
+    //     return std::ranges::min(ch_next_idx);
+    // }
+
+    // thread-safe data for each send task
+    std::vector<std::shared_ptr<Channel>> chs_out;
+    std::vector<size_t> ch_next_idx;  // values are strictly increasing
+    std::vector<coro::event> ch_data_avail;
+
+    std::vector<Node> send_tasks;
+
+    std::vector<Message> recv_messages;
+
+    coro::mutex mtx;
+    coro::condition_variable cv;
+    size_t n_msgs{std::numeric_limits<size_t>::max()};
+};
+
+Node send_task(Context* ctx, UnboundedFanoutState& state, size_t i) {
+    co_await ctx->executor()->schedule();
+
+    // co_await state.data_avail;  // wait for data to be available
+
+    while (true) {
+        // wait for the data to be available
+        co_await state.ch_data_avail[i];
+
+        if (state.all_sent(i)) {
+            // all messages have been sent, nothing else to do
+            break;
+        }
+
+        auto const& msg = state.recv_messages[state.ch_next_idx[i]];
+        // copy msg
+        // msg.content_size
+
+        {
+            auto lock = state.mtx.scoped_lock();
+            state.ch_next_idx[i]++;
+        }
+        co_await state.cv.notify_one();
+
+
+        //
+        if (state.ch_next_idx[i] == state.recv_messages.size() && !state.all_received()) {
+            state.ch_data_avail[i].reset();
+        }
+    }
+}
+
+Node unbounded_fanout(
+    std::shared_ptr<Context> ctx,
+    std::shared_ptr<Channel> ch_in,
+    std::vector<std::shared_ptr<Channel>> chs_out
+) {
+    ShutdownAtExit c{ch_in};
+    ShutdownAtExit c2{chs_out};
+    co_await ctx->executor()->schedule();
+
+    UnboundedFanoutState state(std::move(chs_out));
+
+    size_t purge_idx = 0;
+    while (true) {
+        {
+            auto lock = co_await state.mtx.scoped_lock();
+            co_await state.cv.wait(lock, [&] {
+                return state.recv_messages.size() <= std::ranges::max(state.ch_next_idx);
+            });
+        }
+
+        // n_msgs is only set by this task. So, reading w/o a lock is safe.
+        if (state.n_msgs == std::numeric_limits<size_t>::max()) {
+            auto msg = co_await ch_in->receive();
+            auto lock = co_await state.mtx.scoped_lock();
+            if (msg.empty()) {
+                // no more messages to receive
+                state.n_msgs = state.recv_messages.size();
+            } else {
+                state.recv_messages.push_back(std::move(msg));
+                lock.unlock();
+
+                for (auto& event : state.ch_data_avail) {
+                    event.set();
+                }
+            }
+        }
+
+        size_t last_completed_idx = std::ranges::min(state.ch_next_idx);
+        while (purge_idx <= last_completed_idx) {
+            state.ch_data_avail[purge_idx].reset();
+            purge_idx++;
+        }
+    }
+}
+
+
 }  // namespace
 
 Node fanout(
@@ -53,10 +172,6 @@ Node fanout(
         }
         break;
     case FanoutPolicy::UNBOUNDED:
-        // TODO: Instead of buffering all messages before broadcasting,
-        //       stream them directly by giving each output channel its own
-        //       `coro::queue` and spawning a coroutine per channel that
-        //       sends from that queue.
         {
             // First we receive until the input channel is shutdown.
             std::vector<Message> messages;
