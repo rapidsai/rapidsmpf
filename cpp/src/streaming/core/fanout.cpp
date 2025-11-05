91bd6c77322019b44917c054e68d4932183093bc
niranda perera <niranda.perera@gmail.com>
2025-11-05 13:30:40 -0800

    working draft
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- cpp/src/streaming/core/fanout.cpp
+++ cpp/src/streaming/core/fanout.cpp
@@ -29,24 +29,51 @@ Node send_to_channels(
         // do a reservation for each copy, so that it will fallback to host memory if
         // needed
         auto res = ctx->br()->reserve_or_fail(msg.copy_cost());
-        tasks.push_back(ch_out->send(msg.copy(ctx->br(), res)));
+        tasks.push_back(ch_out->send(msg.copy(res)));
     }
+    coro_results(co_await coro::when_all(std::move(tasks)));
+}
+
+Node bounded_fanout(
+    std::shared_ptr<Context>&& ctx,
+    std::shared_ptr<Channel>&& ch_in,
+    std::vector<std::shared_ptr<Channel>>&& chs_out
+) {
+    ShutdownAtExit c1{ch_in};
+    ShutdownAtExit c2{chs_out};
+    co_await ctx->executor()->schedule();
+
+    while (true) {
+        auto msg = co_await ch_in->receive();
+        if (msg.empty()) {
+            break;
+        }
+        co_await send_to_channels(ctx.get(), msg, chs_out);
+    }
+
+    // Finally, we drain all output channels.
+    std::vector<Node> tasks;
+    tasks.reserve(chs_out.size());
+    std::ranges::transform(chs_out, std::back_inserter(tasks), [&](auto& ch_out) {
+        return ch_out->drain(ctx->executor());
+    });
+
     coro_results(co_await coro::when_all(std::move(tasks)));
 }
 
 Node unbounded_fo_send_task(
     Context& ctx,
-    std::shared_ptr<Channel> const& ch_out,
-    size_t* next_idx,
+    size_t idx,
+    std::vector<std::shared_ptr<Channel>> const& chs_out,
+    std::vector<size_t>& ch_next_idx,
     coro::mutex& mtx,
     coro::condition_variable& data_ready,
     coro::condition_variable& request_data,
     bool const& input_done,
     std::vector<Message> const& recv_messages
 ) {
-    ShutdownAtExit c{ch_out};
+    ShutdownAtExit c{chs_out[idx]};
     co_await ctx.executor()->schedule();
-
     size_t end_idx;
     while (true) {
         {
@@ -55,26 +82,28 @@ Node unbounded_fo_send_task(
                 // irrespective of input_done, update the end_idx to the total number of
                 // messages
                 end_idx = recv_messages.size();
-                return input_done || *next_idx < end_idx;
+                return input_done || ch_next_idx[idx] < end_idx;
             });
-
-            if (input_done && *next_idx == end_idx) {
+            if (input_done && ch_next_idx[idx] == end_idx) {
                 break;
             }
         }
 
         // now we can copy & send messages in indices [next_idx, end_idx)
-        for (size_t i = *next_idx; i < end_idx; i++) {
+        for (size_t i = ch_next_idx[idx]; i < end_idx; i++) {
             auto const& msg = recv_messages[i];
+
+            // make reservations for each message so that it will fallback to host memory
+            // if needed
             auto res = ctx.br()->reserve_or_fail(msg.copy_cost());
-            co_await ch_out->send(msg.copy(ctx.br(), res));
+            co_await chs_out[idx]->send(msg.copy(res));
         }
 
         // now next_idx can be updated to end_idx, and if !input_done, we need to request
         // parent task for more data
         auto lock = co_await mtx.scoped_lock();
-        *next_idx = end_idx;
-        if (input_done) {
+        ch_next_idx[idx] = end_idx;
+        if (input_done && ch_next_idx[idx] == end_idx) {
             break;
         } else {
             lock.unlock();
@@ -82,17 +111,17 @@ Node unbounded_fo_send_task(
         }
     }
 
-    // channels will be drained by the caller
+    co_await chs_out[idx]->drain(ctx.executor());
 }
 
 Node unbounded_fanout(
-    Context& ctx,
-    std::shared_ptr<Channel> const& ch_in,
-    std::vector<std::shared_ptr<Channel>> const& chs_out
+    std::shared_ptr<Context>&& ctx,
+    std::shared_ptr<Channel>&& ch_in,
+    std::vector<std::shared_ptr<Channel>>&& chs_out
 ) {
     ShutdownAtExit c{ch_in};
     ShutdownAtExit c2{chs_out};
-    co_await ctx.executor()->schedule();
+    co_await ctx->executor()->schedule();
 
 
     coro::mutex mtx;
@@ -101,14 +130,16 @@ Node unbounded_fanout(
     bool input_done{false};
     std::vector<Message> recv_messages;
 
-    std::vector<size_t> ch_next_idx{chs_out.size(), 0};
+    std::vector<size_t> ch_next_idx(chs_out.size(), 0);
+
     std::vector<Node> tasks;
     tasks.reserve(chs_out.size());
     for (size_t i = 0; i < chs_out.size(); i++) {
         tasks.emplace_back(unbounded_fo_send_task(
-            ctx,
-            chs_out[i],
-            &ch_next_idx[i],
+            *ctx,
+            i,
+            chs_out,
+            ch_next_idx,
             mtx,
             data_ready,
             request_data,
@@ -129,7 +160,7 @@ Node unbounded_fanout(
             });
         }
 
-        // receive a message from the input channel 
+        // receive a message from the input channel
         auto msg = co_await ch_in->receive();
 
         {  // relock mtx to update input_done/ recv_messages
@@ -147,8 +178,8 @@ Node unbounded_fanout(
         // intentionally not locking the mtx here, because we only need to know a
         // lower-bound on the last completed idx (ch_next_idx values are monotonically
         // increasing)
-        size_t last_completed_idx = std::ranges::min(ch_next_idx) - 1;
-        while (purge_idx < last_completed_idx) {
+        size_t last_completed_idx = std::ranges::min(ch_next_idx);
+        while (purge_idx + 1 < last_completed_idx) {
             recv_messages[purge_idx].reset();
             purge_idx++;
         }
@@ -156,7 +187,6 @@ Node unbounded_fanout(
 
     // Note: there will be some messages to be purged after the loop exits, but we don't
     // need to do anything about them here
-
     coro_results(co_await coro::when_all(std::move(tasks)));
 }
 
@@ -169,37 +199,16 @@ Node fanout(
     std::vector<std::shared_ptr<Channel>> chs_out,
     FanoutPolicy policy
 ) {
-    ShutdownAtExit c1{ch_in};
-    ShutdownAtExit c2{chs_out};
-    co_await ctx->executor()->schedule();
-
     switch (policy) {
     case FanoutPolicy::BOUNDED:
-        while (true) {
-            auto msg = co_await ch_in->receive();
-            if (msg.empty()) {
-                break;
-            }
-            co_await send_to_channels(ctx.get(), msg, chs_out);
-        }
+        co_await bounded_fanout(std::move(ctx), std::move(ch_in), std::move(chs_out));
         break;
     case FanoutPolicy::UNBOUNDED:
-        {
-            co_await unbounded_fanout(*ctx, ch_in, chs_out);
-            break;
-        }
+        co_await unbounded_fanout(std::move(ctx), std::move(ch_in), std::move(chs_out));
+        break;
     default:
         RAPIDSMPF_FAIL("Unknown broadcast policy", std::invalid_argument);
     }
-
-    // Finally, we drain all output channels.
-    std::vector<Node> tasks;
-    tasks.reserve(chs_out.size());
-    std::ranges::transform(chs_out, std::back_inserter(tasks), [&](auto& ch_out) {
-        return ch_out->drain(ctx->executor());
-    });
-    
-    coro_results(co_await coro::when_all(std::move(tasks)));
 }
 
 }  // namespace rapidsmpf::streaming::node
