ec4f0ffec03741362ce0523ace152ce42a29e759
Nate Rock <nrock@nvidia.com>
2025-11-08 08:27:43 -0600

    removed unused RAPIDS_VERSION_MAJOR_MINOR variable

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -16,7 +16,6 @@ rapids-mamba-retry env create -qy -f env.yaml -n checks
 conda activate checks
 
 rapids-logger "fetching cmake-format config"
-RAPIDS_VERSION_MAJOR_MINOR="$(rapids-version-major-minor)"
 FORMAT_FILE_URL=https://raw.githubusercontent.com/rapidsai/rapids-cmake/main/cmake-format-rapids-cmake.json
 export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
 mkdir -p "$(dirname ${RAPIDS_CMAKE_FORMAT_FILE})"
