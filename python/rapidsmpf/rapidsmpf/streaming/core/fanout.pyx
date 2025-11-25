8068f2ec6b209ecf8bb9bf7ab801f4d094bf3dbd
niranda perera <niranda.perera@gmail.com>
2025-11-25 12:32:59 -0800

    Addressing PR comments
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- python/rapidsmpf/rapidsmpf/streaming/core/fanout.pyx
+++ python/rapidsmpf/rapidsmpf/streaming/core/fanout.pyx
@@ -31,8 +31,10 @@ def fanout(Context ctx, Channel ch_in, chs_out, FanoutPolicy policy):
     chs_out
         Output channels to which messages are broadcast.
     policy
-        The fanout strategy to use (see FanoutPolicy).
-
+        The fanout policy to use. `FanoutPolicy.BOUNDED` can be used if all
+        output channels are being consumed by independent consumers in the
+        downstream. `FanoutPolicy.UNBOUNDED` can be used if the output channels
+        are being consumed by a single/ shared consumer in the downstream.
     Returns
     -------
     Streaming node representing the fanout operation.
