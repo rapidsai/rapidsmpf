8db9f36ec24808ec733f70ac320c9b16662fd2c8
Mads R. B. Kristensen <madsbk@gmail.com>
2025-12-01 17:01:22 +0100

    docs and from_uint8_vector

--- cpp/include/rapidsmpf/memory/host_buffer.hpp
+++ cpp/include/rapidsmpf/memory/host_buffer.hpp
@@ -19,83 +19,125 @@
 
 namespace rapidsmpf {
 
+
+/**
+ * @brief Block of host memory allocated.
+ */
 class HostBuffer {
   public:
+    /**
+     * @brief Allocate a new host buffer.
+     *
+     * If `size` is greater than zero, memory is allocated using the provided memory
+     * resource and stream. If `size` is zero, the buffer is created empty.
+     *
+     * @param size Number of bytes to allocate.
+     * @param stream CUDA stream on which allocation and deallocation occur.
+     * @param mr RMM host memory resource used for allocation.
+     */
     HostBuffer(
         std::size_t size, rmm::cuda_stream_view stream, rmm::host_async_resource_ref mr
-    )
-        : stream_{stream}, mr_{mr} {
-        if (size > 0) {
-            auto* ptr = static_cast<std::byte*>(mr_.allocate(stream_, size));
-            span_ = std::span<std::byte>{ptr, size};
-        }
-    }
+    );
 
-    ~HostBuffer() noexcept {
-        deallocate_async();
-    }
-
-    void deallocate_async() noexcept {
-        if (!span_.empty()) {
-            mr_.deallocate(stream_, span_.data(), span_.size());
-        }
-        span_ = {};
-    }
+    ~HostBuffer() noexcept;
 
     // Non copyable
     HostBuffer(HostBuffer const&) = delete;
     HostBuffer& operator=(HostBuffer const&) = delete;
 
-    // Movable
-    HostBuffer(HostBuffer&& other) noexcept
-        : stream_{other.stream_}, mr_{other.mr_}, span_{std::exchange(other.span_, {})} {}
+    /**
+     * @brief Move constructor.
+     *
+     * Transfers ownership of the underlying memory. The moved-from object
+     * becomes empty.
+     *
+     * @param other The buffer to move from.
+     */
+    HostBuffer(HostBuffer&& other) noexcept;
 
-    HostBuffer& operator=(HostBuffer&& other) {
-        if (this != &other) {
-            RAPIDSMPF_EXPECTS(
-                span_.empty(),
-                "cannot move into an already initialized HostBuffer",
-                std::invalid_argument
-            );
-            stream_ = other.stream_;
-            mr_ = other.mr_;
-            span_ = std::exchange(other.span_, {});
-        }
-        return *this;
-    }
+    /**
+     * @brief Move assignment operator.
+     *
+     * Transfers ownership of the underlying memory. The current buffer must be
+     * empty before assignment.
+     *
+     * @param other The buffer to move from.
+     * @return Reference to this object.
+     *
+     * @throws std::invalid_argument if this buffer is already initialized.
+     */
+    HostBuffer& operator=(HostBuffer&& other);
 
-    [[nodiscard]] rmm::cuda_stream_view stream() const noexcept {
-        return stream_;
-    }
+    /**
+     * @brief Stream-ordered deallocates the buffer, if allocated.
+     *
+     * After deallocation the buffer becomes empty. It is safe to call this
+     * method multiple times.
+     */
+    void deallocate_async() noexcept;
 
-    [[nodiscard]] std::size_t size() const noexcept {
-        return span_.size();
-    }
+    /**
+     * @brief Get the CUDA stream associated with this buffer.
+     *
+     * @return CUDA stream view.
+     */
+    [[nodiscard]] rmm::cuda_stream_view stream() const noexcept;
 
-    [[nodiscard]] bool empty() const noexcept {
-        return span_.empty();
-    }
+    /**
+     * @brief Get the size of the buffer in bytes.
+     *
+     * @return Number of bytes in the buffer.
+     */
+    [[nodiscard]] std::size_t size() const noexcept;
 
-    [[nodiscard]] std::byte* data() noexcept {
-        return span_.data();
-    }
+    /**
+     * @brief Check whether the buffer is empty.
+     *
+     * @return True if no memory is allocated.
+     */
+    [[nodiscard]] bool empty() const noexcept;
 
-    [[nodiscard]] std::byte const* data() const noexcept {
-        return span_.data();
-    }
+    /**
+     * @brief Get a pointer to the buffer data.
+     *
+     * @return Pointer to the underlying memory.
+     */
+    [[nodiscard]] std::byte* data() noexcept;
 
-    // Mainly for debugging and testing, not performant.
-    [[nodiscard]] std::vector<std::uint8_t> copy_to_uint8_vector() const {
-        std::vector<std::uint8_t> ret(size());
-        if (!empty()) {
-            stream_.synchronize();
-            RAPIDSMPF_CUDA_TRY(
-                cudaMemcpyAsync(ret.data(), data(), size(), cudaMemcpyDefault, stream_)
-            );
-            stream_.synchronize();
-        }
-        return ret;
-    };
+    /**
+     * @brief Get a const pointer to the buffer data.
+     *
+     * @return Const pointer to the underlying memory.
+     */
+    [[nodiscard]] std::byte const* data() const noexcept;
+
+    /**
+     * @brief Copy the contents of the buffer into a host `std::vector`.
+     *
+     * This is primarily intended for debugging or testing. It performs a
+     * stream synchronization before and after the copy.
+     *
+     * @return A vector containing the copied bytes.
+     */
+    [[nodiscard]] std::vector<std::uint8_t> copy_to_uint8_vector() const;
+
+    /**
+     * @brief Construct a `HostBuffer` by copying data from a `std::vector<std::uint8_t>`.
+     *
+     * A new buffer is allocated using the provided memory resource and stream.
+     * This helper is intended for tests and small debug utilities.
+     *
+     * @param data Source vector containing the bytes to copy.
+     * @param stream CUDA stream used for allocation and copy.
+     * @param mr Host memory resource used to allocate the buffer.
+     *
+     * @return A new `HostBuffer` containing a copy of `data`.
+     */
+    static HostBuffer from_uint8_vector(
+        std::vector<std::uint8_t> const& data,
+        rmm::cuda_stream_view stream,
+        rmm::host_async_resource_ref mr
+    );
 
   private:
     rmm::cuda_stream_view stream_;
