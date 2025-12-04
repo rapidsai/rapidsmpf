50a050a7ae01d680e7e66a039f12938fbf9dd7be
Lawrence Mitchell <lmitchell@nvidia.com>
2025-12-04 17:34:29 +0000

    And in q1

--- cpp/benchmarks/streaming/ndsh/q01.cpp
+++ cpp/benchmarks/streaming/ndsh/q01.cpp
@@ -8,53 +8,31 @@
 #include <chrono>
 #include <cstdlib>
 #include <filesystem>
-#include <iostream>
 #include <memory>
 #include <optional>
 
 #include <cuda_runtime_api.h>
-#include <getopt.h>
 #include <mpi.h>
 
 #include <cudf/aggregation.hpp>
 #include <cudf/ast/expressions.hpp>
 #include <cudf/binaryop.hpp>
-#include <cudf/concatenate.hpp>
-#include <cudf/copying.hpp>
-#include <cudf/datetime.hpp>
 #include <cudf/groupby.hpp>
 #include <cudf/io/parquet.hpp>
 #include <cudf/io/types.hpp>
-#include <cudf/merge.hpp>
-#include <cudf/round.hpp>
 #include <cudf/scalar/scalar.hpp>
-#include <cudf/scalar/scalar_factories.hpp>
 #include <cudf/sorting.hpp>
-#include <cudf/stream_compaction.hpp>
-#include <cudf/strings/find.hpp>
 #include <cudf/table/table.hpp>
-#include <cudf/table/table_view.hpp>
 #include <cudf/transform.hpp>
 #include <cudf/types.hpp>
-#include <cudf/unary.hpp>
 #include <cudf/wrappers/timestamps.hpp>
 #include <rmm/mr/cuda_async_memory_resource.hpp>
-#include <rmm/mr/per_device_resource.hpp>
-#include <rmm/mr/pool_memory_resource.hpp>
-#include <rmm/resource_ref.hpp>
 
-#include <rapidsmpf/buffer/buffer.hpp>
-#include <rapidsmpf/buffer/packed_data.hpp>
 #include <rapidsmpf/communicator/communicator.hpp>
 #include <rapidsmpf/communicator/mpi.hpp>
-#include <rapidsmpf/communicator/single.hpp>
-#include <rapidsmpf/communicator/ucxx.hpp>
-#include <rapidsmpf/communicator/ucxx_utils.hpp>
-#include <rapidsmpf/config.hpp>
-#include <rapidsmpf/cuda_event.hpp>
 #include <rapidsmpf/integrations/cudf/partition.hpp>
+#include <rapidsmpf/memory/packed_data.hpp>
 #include <rapidsmpf/nvtx.hpp>
-#include <rapidsmpf/statistics.hpp>
 #include <rapidsmpf/streaming/coll/allgather.hpp>
 #include <rapidsmpf/streaming/core/channel.hpp>
 #include <rapidsmpf/streaming/core/context.hpp>
@@ -63,11 +41,7 @@
 #include <rapidsmpf/streaming/cudf/table_chunk.hpp>
 
 #include "concatenate.hpp"
-#include "join.hpp"
-#include "rapidsmpf/cuda_stream.hpp"
-#include "rapidsmpf/owning_wrapper.hpp"
-#include "rapidsmpf/streaming/core/coro_utils.hpp"
-#include "utilities.hpp"
+#include "utils.hpp"
 
 // select
 //     l_orderkey,
@@ -351,16 +325,9 @@ std::string get_table_path(
         auto packed_data =
             co_await gatherer.extract_all(rapidsmpf::streaming::AllGather::Ordered::NO);
         if (ctx->comm()->rank() == 0) {
-            std::vector<rapidsmpf::PackedData> chunks;
-            chunks.reserve(packed_data.size());
-            std::ranges::transform(
-                packed_data, std::back_inserter(chunks), [](auto& chunk) {
-                    return std::move(chunk.data);
-                }
-            );
             auto global_result = rapidsmpf::unpack_and_concat(
                 rapidsmpf::unspill_partitions(
-                    std::move(chunks), ctx->br(), true, ctx->statistics()
+                    std::move(packed_data), ctx->br(), true, ctx->statistics()
                 ),
                 chunk_stream,
                 ctx->br(),
@@ -678,232 +645,84 @@ static __device__ void calculate_charge(double *charge, double discprice, double
 }
 }  // namespace
 
-struct ProgramOptions {
-    int num_streaming_threads{1};
-    int num_iterations{2};
-    cudf::size_type num_rows_per_chunk{100'000'000};
-    std::optional<double> spill_device_limit{std::nullopt};
-    bool use_shuffle_join = false;
-    std::string output_file;
-    std::string input_directory;
-};
-
-ProgramOptions parse_options(int argc, char** argv) {
-    ProgramOptions options;
-
-    auto print_usage = [&argv]() {
-        std::cerr
-            << "Usage: " << argv[0] << " [options]\n"
-            << "Options:\n"
-            << "  --num-streaming-threads <n>  Number of streaming threads (default: 1)\n"
-            << "  --num-iterations <n>         Number of iterations (default: 2)\n"
-            << "  --num-rows-per-chunk <n>     Number of rows per chunk (default: "
-               "100000000)\n"
-            << "  --spill-device-limit <n>     Fractional spill device limit (default: "
-               "None)\n"
-            << "  --use-shuffle-join           Use shuffle join (default: false)\n"
-            << "  --output-file <path>         Output file path (required)\n"
-            << "  --input-directory <path>     Input directory path (required)\n"
-            << "  --help                       Show this help message\n";
-    };
-
-    static struct option long_options[] = {
-        {"num-streaming-threads", required_argument, nullptr, 1},
-        {"num-rows-per-chunk", required_argument, nullptr, 2},
-        {"use-shuffle-join", no_argument, nullptr, 3},
-        {"output-file", required_argument, nullptr, 4},
-        {"input-directory", required_argument, nullptr, 5},
-        {"help", no_argument, nullptr, 6},
-        {"spill-device-limit", required_argument, nullptr, 7},
-        {"num-iterations", required_argument, nullptr, 8},
-        {nullptr, 0, nullptr, 0}
-    };
-
-    int opt;
-    int option_index = 0;
-
-    bool saw_output_file = false;
-    bool saw_input_directory = false;
-
-    while ((opt = getopt_long(argc, argv, "", long_options, &option_index)) != -1) {
-        switch (opt) {
-        case 1:
-            options.num_streaming_threads = std::atoi(optarg);
-            break;
-        case 2:
-            options.num_rows_per_chunk = std::atoi(optarg);
-            break;
-        case 3:
-            options.use_shuffle_join = true;
-            break;
-        case 4:
-            options.output_file = optarg;
-            saw_output_file = true;
-            break;
-        case 5:
-            options.input_directory = optarg;
-            saw_input_directory = true;
-            break;
-        case 6:
-            print_usage();
-            std::exit(0);
-        case 7:
-            options.spill_device_limit = std::stod(optarg);
-            break;
-        case 8:
-            options.num_iterations = std::atoi(optarg);
-            break;
-        case '?':
-            if (optopt == 0 && optind > 1) {
-                std::cerr << "Error: Unknown option '" << argv[optind - 1] << "'\n\n";
-            }
-            print_usage();
-            std::exit(1);
-        default:
-            print_usage();
-            std::exit(1);
-        }
-    }
-
-    // Check if required options were provided
-    if (!saw_output_file || !saw_input_directory) {
-        if (!saw_output_file) {
-            std::cerr << "Error: --output-file is required\n";
-        }
-        if (!saw_input_directory) {
-            std::cerr << "Error: --input-directory is required\n";
-        }
-        std::cerr << std::endl;
-        print_usage();
-        std::exit(1);
-    }
-
-    return options;
-}
-
 int main(int argc, char** argv) {
     cudaFree(nullptr);
-    rapidsmpf::mpi::init(&argc, &argv);
-    MPI_Comm mpi_comm;
-    RAPIDSMPF_MPI(MPI_Comm_dup(MPI_COMM_WORLD, &mpi_comm));
-    auto cmd_options = parse_options(argc, argv);
-    auto limit_size = rmm::percent_of_free_device_memory(
-        static_cast<std::size_t>(cmd_options.spill_device_limit.value_or(1) * 100)
-    );
-    rmm::mr::cuda_async_memory_resource mr{};
-    // rmm::mr::cuda_memory_resource base{};
-    // rmm::mr::pool_memory_resource mr{&base, pool_size};
-    auto stats_mr = rapidsmpf::RmmResourceAdaptor(&mr);
-    rmm::device_async_resource_ref mr_ref(stats_mr);
-    rmm::mr::set_current_device_resource(&stats_mr);
-    rmm::mr::set_current_device_resource_ref(mr_ref);
-    std::unordered_map<rapidsmpf::MemoryType, rapidsmpf::BufferResource::MemoryAvailable>
-        memory_available{};
-    if (cmd_options.spill_device_limit.has_value()) {
-        memory_available[rapidsmpf::MemoryType::DEVICE] = rapidsmpf::LimitAvailableMemory{
-            &stats_mr, static_cast<std::int64_t>(limit_size)
-        };
-    }
-    auto br = std::make_shared<rapidsmpf::BufferResource>(
-        stats_mr, std::move(memory_available)
-    );
-    auto envvars = rapidsmpf::config::get_environment_variables();
-    envvars["num_streaming_threads"] = std::to_string(cmd_options.num_streaming_threads);
-    auto options = rapidsmpf::config::Options(envvars);
-    auto stats = std::make_shared<rapidsmpf::Statistics>(&stats_mr);
-    {
-        auto comm = rapidsmpf::ucxx::init_using_mpi(mpi_comm, options);
-        auto progress =
-            std::make_shared<rapidsmpf::ProgressThread>(comm->logger(), stats);
-        auto ctx =
-            std::make_shared<rapidsmpf::streaming::Context>(options, comm, br, stats);
-        comm->logger().print(
-            "Executor has ", ctx->executor()->thread_count(), " threads"
-        );
-        comm->logger().print("Executor has ", ctx->comm()->nranks(), " ranks");
+    auto mr = rmm::mr::cuda_async_memory_resource{};
+    auto stats_wrapper = rapidsmpf::RmmResourceAdaptor(&mr);
+    auto arguments = rapidsmpf::ndsh::parse_arguments(argc, argv);
+    auto ctx = rapidsmpf::ndsh::create_context(arguments, &stats_wrapper);
+    std::string output_path = arguments.output_file;
+    std::vector<double> timings;
+    for (int i = 0; i < arguments.num_iterations; i++) {
+        int op_id = 0;
+        std::vector<rapidsmpf::streaming::Node> nodes;
+        auto start = std::chrono::steady_clock::now();
+        {
+            RAPIDSMPF_NVTX_SCOPED_RANGE("Constructing Q1 pipeline");
 
-        std::string output_path = cmd_options.output_file;
-        std::vector<double> timings;
-        [[maybe_unused]] int op_id = 0;
-        for (int i = 0; i < cmd_options.num_iterations; i++) {
-            std::vector<rapidsmpf::streaming::Node> nodes;
-            auto start = std::chrono::steady_clock::now();
-            {
-                RAPIDSMPF_NVTX_SCOPED_RANGE("Constructing Q1 pipeline");
+            // Input data channels
+            auto lineitem = ctx->create_channel();
+            // Out: l_returnflag, l_linestatus, l_quantity, l_extendedprice,
+            // l_discount, l_tax
+            nodes.push_back(read_lineitem(
+                ctx,
+                lineitem,
+                /* num_tickets */ 4,
+                arguments.num_rows_per_chunk,
+                arguments.input_directory
+            ));
 
-                // Input data channels
-                auto lineitem = ctx->create_channel();
-                // Out: l_returnflag, l_linestatus, l_quantity, l_extendedprice,
-                // l_discount, l_tax
-                nodes.push_back(read_lineitem(
-                    ctx,
-                    lineitem,
-                    /* num_tickets */ 4,
-                    cmd_options.num_rows_per_chunk,
-                    cmd_options.input_directory
-                ));
-
-                auto groupby_input = ctx->create_channel();
-                // Out: l_returnflag, l_linestatus, l_quantity, l_extendedprice,
-                // disc_price = (l_extendedprice * (1 - l_discount)),
-                // charge = (l_extendedprice * (1 - l_discount) * (1 + l_tax))
-                // l_discount
-                nodes.push_back(select_columns_for_groupby(ctx, lineitem, groupby_input));
-                auto chunkwise_groupby = ctx->create_channel();
-                nodes.push_back(
-                    chunkwise_groupby_agg(ctx, groupby_input, chunkwise_groupby)
-                );
-                auto final_groupby_input = ctx->create_channel();
-                nodes.push_back(
-                    rapidsmpf::ndsh::concatenate(
-                        ctx, chunkwise_groupby, final_groupby_input
-                    )
-                );
-                auto groupby_output = ctx->create_channel();
-                nodes.push_back(final_groupby_agg(
-                    ctx,
-                    final_groupby_input,
-                    groupby_output,
-                    static_cast<rapidsmpf::OpID>(10 * i + op_id++)
-                ));
-                auto sorted = ctx->create_channel();
-                nodes.push_back(sort_by(ctx, groupby_output, sorted));
-                nodes.push_back(write_parquet(ctx, sorted, output_path));
-            }
-            auto end = std::chrono::steady_clock::now();
-            std::chrono::duration<double> pipeline = end - start;
-            start = std::chrono::steady_clock::now();
-            {
-                RAPIDSMPF_NVTX_SCOPED_RANGE("Q3 Iteration");
-                rapidsmpf::streaming::run_streaming_pipeline(std::move(nodes));
-            }
-            end = std::chrono::steady_clock::now();
-            std::chrono::duration<double> compute = end - start;
-            comm->logger().print(
-                "Iteration ", i, " pipeline construction time [s]: ", pipeline.count()
+            auto groupby_input = ctx->create_channel();
+            // Out: l_returnflag, l_linestatus, l_quantity, l_extendedprice,
+            // disc_price = (l_extendedprice * (1 - l_discount)),
+            // charge = (l_extendedprice * (1 - l_discount) * (1 + l_tax))
+            // l_discount
+            nodes.push_back(select_columns_for_groupby(ctx, lineitem, groupby_input));
+            auto chunkwise_groupby = ctx->create_channel();
+            nodes.push_back(chunkwise_groupby_agg(ctx, groupby_input, chunkwise_groupby));
+            auto final_groupby_input = ctx->create_channel();
+            nodes.push_back(
+                rapidsmpf::ndsh::concatenate(ctx, chunkwise_groupby, final_groupby_input)
             );
-            comm->logger().print("Iteration ", i, " compute time [s]: ", compute.count());
-            timings.push_back(pipeline.count());
-            timings.push_back(compute.count());
-            ctx->comm()->logger().print(stats->report());
-            RAPIDSMPF_MPI(MPI_Barrier(mpi_comm));
+            auto groupby_output = ctx->create_channel();
+            nodes.push_back(final_groupby_agg(
+                ctx,
+                final_groupby_input,
+                groupby_output,
+                static_cast<rapidsmpf::OpID>(10 * i + op_id++)
+            ));
+            auto sorted = ctx->create_channel();
+            nodes.push_back(sort_by(ctx, groupby_output, sorted));
+            nodes.push_back(write_parquet(ctx, sorted, output_path));
         }
-        if (comm->rank() == 0) {
-            for (int i = 0; i < cmd_options.num_iterations; i++) {
-                comm->logger().print(
-                    "Iteration ",
-                    i,
-                    " pipeline construction time [s]: ",
-                    timings[size_t(2 * i)]
-                );
-                comm->logger().print(
-                    "Iteration ", i, " compute time [s]: ", timings[size_t(2 * i + 1)]
-                );
-            }
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> pipeline = end - start;
+        start = std::chrono::steady_clock::now();
+        {
+            RAPIDSMPF_NVTX_SCOPED_RANGE("Q1 Iteration");
+            rapidsmpf::streaming::run_streaming_pipeline(std::move(nodes));
+        }
+        end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> compute = end - start;
+        timings.push_back(pipeline.count());
+        timings.push_back(compute.count());
+        ctx->comm()->logger().print(ctx->statistics()->report());
+    }
+    if (ctx->comm()->rank() == 0) {
+        for (int i = 0; i < arguments.num_iterations; i++) {
+            ctx->comm()->logger().print(
+                "Iteration ",
+                i,
+                " pipeline construction time [s]: ",
+                timings[size_t(2 * i)]
+            );
+            ctx->comm()->logger().print(
+                "Iteration ", i, " compute time [s]: ", timings[size_t(2 * i + 1)]
+            );
         }
     }
-
-    RAPIDSMPF_MPI(MPI_Comm_free(&mpi_comm));
-    RAPIDSMPF_MPI(MPI_Finalize());
+    if (rapidsmpf::mpi::is_initialized()) {
+        RAPIDSMPF_MPI(MPI_Finalize());
+    }
     return 0;
 }
