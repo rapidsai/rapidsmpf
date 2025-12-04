b7877166ab4682db3f5838dd4340cc95bea78768
Peter Andreas Entschev <peter@entschev.com>
2025-12-04 06:21:25 -0800

    More style fixes

--- cpp/include/rapidsmpf/bootstrap/ucxx.hpp
+++ cpp/include/rapidsmpf/bootstrap/ucxx.hpp
@@ -38,8 +38,8 @@ bool is_running_with_rrun();
  * The number of ranks is fetched from the `RAPIDSMPF_NRANKS` environment variable.
  *
  * @return Number of ranks.
- * @throws std::runtime_error if not running with `rrun` or if `RAPIDSMPF_NRANKS` is not set
- * or cannot be parsed.
+ * @throws std::runtime_error if not running with `rrun` or if `RAPIDSMPF_NRANKS` is not
+ * set or cannot be parsed.
  */
 std::int32_t get_nranks();
 
