2bb10045d53f554094fd5714570347f8d057d810
rjzamora <rzamora217@gmail.com>
2025-12-09 08:48:29 -0800

    drop gill for cpp_insert_chunk_into_partition_map as well

--- python/rapidsmpf/rapidsmpf/streaming/coll/shuffler.pyx
+++ python/rapidsmpf/rapidsmpf/streaming/coll/shuffler.pyx
@@ -224,13 +224,18 @@ cdef class ShufflerAsync:
              that partition.
         """
         cdef unordered_map[uint32_t, cpp_PackedData] c_chunks
+        cdef PackedData pd
+        cdef uint32_t c_pid
         c_chunks.reserve(len(chunks))
         for pid, chunk in chunks.items():
-            if not (<PackedData?>chunk).c_obj:
+            pd = <PackedData>chunk
+            c_pid = <uint32_t>pid
+            if not pd.c_obj:
                 raise ValueError("PackedData was empty")
-            cpp_insert_chunk_into_partition_map(
-                c_chunks, pid, move((<PackedData>chunk).c_obj)
-            )
+            with nogil:
+                cpp_insert_chunk_into_partition_map(
+                    c_chunks, c_pid, move(pd.c_obj)
+                )
         with nogil:
             deref(self._handle).insert(move(c_chunks))
 
