7c945d5b4748f114406c5b9cdfff3097e909ed2c
ptaylor <paul.e.taylor@me.com>
2025-12-10 02:13:47 +0000

    add get_kvikio.cmake and get_nvcomp.cmake

---
+++ cmake/thirdparty/get_nvcomp.cmake
@@ -0,0 +1,24 @@
+# =============================================================================
+# cmake-format: off
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+# cmake-format: on
+# =============================================================================
+
+# This function finds nvcomp and sets any additional necessary environment variables.
+function(find_and_configure_nvcomp)
+
+  include(${rapids-cmake-dir}/cpm/nvcomp.cmake)
+  set(export_args)
+  if(RAPIDSMPF_EXPORT_NVCOMP)
+    set(export_args BUILD_EXPORT_SET rapidsmpf-exports INSTALL_EXPORT_SET rapidsmpf-exports)
+  endif()
+  rapids_cpm_nvcomp(${export_args} USE_PROPRIETARY_BINARY ON)
+
+  # Per-thread default stream
+  if(TARGET nvcomp AND RAPIDSMPF_USE_PER_THREAD_DEFAULT_STREAM)
+    target_compile_definitions(nvcomp PRIVATE CUDA_API_PER_THREAD_DEFAULT_STREAM)
+  endif()
+endfunction()
+
+find_and_configure_nvcomp()
