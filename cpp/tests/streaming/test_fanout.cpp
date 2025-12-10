a2ad5547431e5bd62388a5a0cf320f92c245b10c
niranda perera <niranda.perera@gmail.com>
2025-12-10 10:24:47 -0800

    remove cout
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- cpp/tests/streaming/test_fanout.cpp
+++ cpp/tests/streaming/test_fanout.cpp
@@ -444,15 +444,6 @@ Node many_input_sink(
                 if (msg.empty()) {
                     break;
                 }
-                std::cout << msg.content_description().principal_memory_type() << " "
-                          << msg.content_description().content_size(
-                                 rapidsmpf::MemoryType::DEVICE
-                             )
-                          << " "
-                          << msg.content_description().content_size(
-                                 rapidsmpf::MemoryType::HOST
-                             )
-                          << std::endl;
                 outs[i].push_back(std::move(msg));
             }
         }
