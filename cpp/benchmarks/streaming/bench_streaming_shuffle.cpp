d026b7acc3a778fb08b9d2b45b566698b016301c
Mads R. B. Kristensen <madsbk@gmail.com>
2025-12-14 11:08:06 +0100

    doc

--- cpp/benchmarks/streaming/bench_streaming_shuffle.cpp
+++ cpp/benchmarks/streaming/bench_streaming_shuffle.cpp
@@ -73,7 +73,7 @@ class ArgumentParser {
                            << "  -m <mr>    RMM memory resource {cuda, pool, async, "
                               "managed} (default: pool)\n"
                            << "  -l <num>   Device memory limit in MiB (default:-1, "
-                              "disabled)\n"
+                              "unlimited)\n"
                            << "  -L         Disable Pinned host memory (default: "
                               " unlimited)\n"
                            << "  -x         Enable memory profiler (default: disabled)\n"
