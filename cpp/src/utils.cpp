ac18e66d01db692d0068c40e619bc85ada619b59
Mads R. B. Kristensen <madsbk@gmail.com>
2026-01-08 12:29:22 -0800

    System info utils (#743)
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/rapidsmpf/pull/743

--- cpp/src/utils.cpp
+++ cpp/src/utils.cpp
@@ -1,14 +1,10 @@
 /**
- * SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <algorithm>
-
-#if RAPIDSMPF_HAVE_NUMA
-#include <numa.h>
-#include <sched.h>
-#endif
+#include <ranges>
 
 #include <rapidsmpf/error.hpp>
 #include <rapidsmpf/utils.hpp>
@@ -73,25 +69,6 @@ bool parse_string(std::string const& value) {
         return false;
     }
     throw std::invalid_argument("cannot parse \"" + std::string{value} + "\"");
-}
-
-int get_current_numa_node_id() {
-#if RAPIDSMPF_HAVE_NUMA
-    static const int numa_node_id = [] {
-        RAPIDSMPF_EXPECTS(
-            numa_available() != -1, "NUMA is not available", std::runtime_error
-        );
-        int cpu = sched_getcpu();
-        int numa_node = numa_node_of_cpu(cpu);
-        RAPIDSMPF_EXPECTS(
-            numa_node >= 0, "failed to get NUMA node ID", std::runtime_error
-        );
-        return numa_node;
-    }();
-    return numa_node_id;
-#else
-    return 0;
-#endif
 }
 
 }  // namespace rapidsmpf
