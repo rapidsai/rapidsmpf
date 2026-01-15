ba138b04ce739cacacbabfaf4305ce9302cd8870
Jake Awe <jawe@nvidia.com>
2026-01-15 14:31:47 -0600

    Prepare release/26.02

--- cmake/thirdparty/get_cudf.cmake
+++ cmake/thirdparty/get_cudf.cmake
@@ -43,5 +43,5 @@ function(find_and_configure_cudf)
   endif()
 endfunction()
 find_and_configure_cudf(
-  VERSION ${RAPIDS_VERSION} GIT_REPO https://github.com/rapidsai/cudf.git GIT_TAG "main"
+  VERSION ${RAPIDS_VERSION} GIT_REPO https://github.com/rapidsai/cudf.git GIT_TAG "release/26.02"
 )
