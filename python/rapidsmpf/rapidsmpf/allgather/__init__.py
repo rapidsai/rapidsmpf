0253aae01139f88e9828a7e065fd88c7e11fdb29
rjzamora <rzamora217@gmail.com>
2026-01-27 07:40:27 -0800

    drop unintended change

--- python/rapidsmpf/rapidsmpf/allgather/__init__.py
+++ python/rapidsmpf/rapidsmpf/allgather/__init__.py
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
-"""Collective communication interfaces."""
+"""AllGather communication interfaces."""
 
 from __future__ import annotations
 
