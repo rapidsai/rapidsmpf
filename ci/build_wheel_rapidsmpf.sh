5c87a747646aedc6584ede1f6fa13f0ab0b3ec6c
Peter Andreas Entschev <peter@entschev.com>
2026-01-30 23:08:47 +0100

    Use `TopologyDiscovery` from cuCascade (#774)
    Add cuCascade as a dependency to RapidsMPF, allowing removal of `TopologyDiscovery` implementation from RapidsMPF and use the one from cuCascade.
    
    Authors:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rapidsmpf/pull/774

--- ci/build_wheel_rapidsmpf.sh
+++ ci/build_wheel_rapidsmpf.sh
@@ -45,6 +45,7 @@ export SITE_PACKAGES
 
 python -m auditwheel repair \
     --exclude libcudf.so \
+    --exclude libnvidia-ml.so.1 \
     --exclude librapids_logger.so \
     --exclude librmm.so \
     --exclude libucp.so.0 \
