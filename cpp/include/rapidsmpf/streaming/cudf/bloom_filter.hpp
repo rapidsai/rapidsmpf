7f7d32ef016a53de3072e45af728b9778f6feb71
Lawrence Mitchell <lmitchell@nvidia.com>
2026-02-02 12:45:19 +0000

    Better docstring

--- cpp/include/rapidsmpf/streaming/cudf/bloom_filter.hpp
+++ cpp/include/rapidsmpf/streaming/cudf/bloom_filter.hpp
@@ -17,14 +17,27 @@
 namespace rapidsmpf::streaming {
 
 /**
- * @brief Utility management for construction and use of a bloom filter.
+ * @brief Utility managing construction and use of a bloom filter.
  *
  * This class provides methods to build a bloom filter from a stream of `TableChunk`s and
  * then apply that filter to a different stream.
+ *
+ * A bloom filter is a fixed size probabilistic data structure that provides approximate
+ * set membership queries with no false negatives. That is, let `A` be some set and `f(A)`
+ * be the bloom filter representation of that set. Then, for all `a ∈ A` it holds that `a
+ * ∈ f(A)`. Conversely, there is a false positive rate that increases with the number of
+ * distinct values inserted into the bloom filter, and decreases with the number of filter
+ * blocks. That is, for any given bloom filter, there exists `a ∉ A` such that `a ∈ f(A)`.
+ *
+ * See https://arxiv.org/pdf/2512.15595 for details on the GPU implementation used.
+ *
+ * We use bloom filters to provide runtime pre-filtering of tables during shuffle-based
+ * joins. We gather the keys that will match from the build side and use those to
+ * pre-filter the probe side before shuffling.
  */
 struct BloomFilter {
     /**
-     * @brief Construct a bloom filter.
+     * @brief Construct storage for a bloom filter.
      *
      * @param ctx Streaming context. The construction of the filter will be collective
      * over this context.
