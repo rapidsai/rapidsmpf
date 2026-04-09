7c2df573e7318fbfdad929183f5462cd2e128a05
jakirkham <jakirkham@gmail.com>
2026-04-09 03:49:29 -0700

    Wrap `broadcast_listener_address` to fix lint

--- cpp/src/communicator/ucxx_utils.cpp
+++ cpp/src/communicator/ucxx_utils.cpp
@@ -31,7 +31,9 @@ namespace {
  * @param listener_address object containing the listener address of the root,
  * which will be read from in rank 0 and stored to in all other ranks.
  */
-void broadcast_listener_address(MPI_Comm mpi_comm, std::string_view root_worker_address_str) {
+void broadcast_listener_address(
+    MPI_Comm mpi_comm, std::string_view root_worker_address_str
+) {
     std::size_t address_size{root_worker_address_str.size()};
 
     RAPIDSMPF_MPI(
