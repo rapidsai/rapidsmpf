4dc1c40a2ab13e8e17005e68e8a7e8af9a5dcf9f
Vyas Ramasubramani <vyasr@nvidia.com>
2026-06-05 15:48:27 +0000

    Use topology-only build of cucascade

--- cmake/thirdparty/get_cucascade.cmake
+++ cmake/thirdparty/get_cucascade.cmake
@@ -11,16 +11,17 @@
 function(find_and_configure_cucascade)
   rapids_cpm_find(
     cuCascade 0.1.0
-    GLOBAL_TARGETS cuCascade::cucascade cuCascade::cucascade_topology_discovery
+    GLOBAL_TARGETS cuCascade::cucascade_topology_discovery
     CPM_ARGS
     GIT_REPOSITORY https://github.com/NVIDIA/cuCascade.git
-    GIT_TAG main
+    GIT_TAG 6a8cf0f7c545c601cec4b41a0da22ad6a17eeb7c
     GIT_SHALLOW FALSE
     OPTIONS "CUCASCADE_BUILD_TESTS OFF"
             "CUCASCADE_BUILD_BENCHMARKS OFF"
             "CUCASCADE_BUILD_SHARED_LIBS OFF"
             "CUCASCADE_BUILD_STATIC_LIBS ON"
             "CUCASCADE_WARNINGS_AS_ERRORS OFF"
+            "CUCASCADE_TOPOLOGY_ONLY ON"
     EXCLUDE_FROM_ALL
   )
 endfunction()
