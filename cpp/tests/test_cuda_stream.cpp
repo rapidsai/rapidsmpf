da18ba1100a9abe73a290dd554327e9aaa5eec76
Minh Vu <vuhoangminh97@gmail.com>
2026-06-15 20:58:38 +0200

    Apply clang-format

--- cpp/tests/test_cuda_stream.cpp
+++ cpp/tests/test_cuda_stream.cpp
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <utility>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-
-#include <utility>
 
 #include <rmm/cuda_stream.hpp>
 #include <rmm/cuda_stream_view.hpp>
