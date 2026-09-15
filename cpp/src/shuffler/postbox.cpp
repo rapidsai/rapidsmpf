d863ef2b360f5c80941ace698058abfbb26a182a
niranda perera <niranda.perera@gmail.com>
2026-09-15 10:12:50 -0700

    chunks extract and restore
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- cpp/src/shuffler/postbox.cpp
+++ cpp/src/shuffler/postbox.cpp
@@ -33,6 +33,38 @@ std::vector<Chunk> ChunksToSend::extract_ready() {
     return result;
 }
 
+std::vector<Chunk> ChunksToSend::extract_and_restore(
+    BufferResource* br, std::span<MemoryType const> memory_types
+) {
+    std::lock_guard lock(mutex_);
+    std::vector<Chunk> result;
+    for (auto&& chunk : chunks_) {
+        if (!chunk->is_ready()) {
+            break;
+        }
+        auto const restore = chunk->is_on_disk();
+        if (restore) {
+            auto reservation = br->try_reserve_or_spill(chunk->data_size(), memory_types);
+            RAPIDSMPF_EXPECTS(
+                reservation.has_value(),
+                "failed to reserve addressable memory for an outgoing disk-backed chunk",
+                std::runtime_error
+            );
+            chunk->set_data_buffer(br->move(chunk->release_data_buffer(), *reservation));
+        }
+        auto c = std::move(chunk);
+        result.emplace_back(std::move(*c));
+        if (restore) {
+            // break after the first disk-backed chunk is restored to addressable memory
+            // because restoring is slow and introduce memory pressure for addressable
+            // memory.
+            break;
+        }
+    }
+    std::erase(chunks_, nullptr);
+    return result;
+}
+
 bool ChunksToSend::empty() const {
     std::lock_guard lock(mutex_);
     return chunks_.empty();
