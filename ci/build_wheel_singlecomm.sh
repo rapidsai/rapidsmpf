5da31bfe1f616eb1ba239969be79f7bd66d34e18
Kyle Edwards <kyedwards@nvidia.com>
2026-09-16 12:41:34 -0400

    copyright

--- ci/build_wheel_singlecomm.sh
+++ ci/build_wheel_singlecomm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # Tests building without MPI and without UCXX. This script only ensures the build
